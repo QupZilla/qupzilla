bc1b6fb947f2079732da40333dc1844c711df4bc
nowrep <nowrep@gmail.com>
2011-03-03 15:26:02 +0100

    deleted tools_ folder

--- tools_/updatePluginTranslations.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-/home/david/Programování/qtsdk-2010.05/qt/bin/lupdate /home/david/Programování/Qt\ C++/QupZilla/plugins/TestPlugin/src/TestPlugin.pro -no-obsolete -ts /home/david/Programování/Qt\ C++/QupZilla/plugins/TestPlugin/cs_CZ.ts
-
-/home/david/Programování/qtsdk-2010.05/qt/bin/lupdate /home/david/Programování/Qt\ C++/QupZilla/plugins/TestPlugin/src/TestPlugin.pro -no-obsolete -ts /home/david/Programování/Qt\ C++/QupZilla/plugins/TestPlugin/sk_SK.ts
-
-read -p "Press [ENTER] to close terminal"
-exit
