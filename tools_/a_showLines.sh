5f946a8e209711814b0b560ab26f26db2bd859a4
nowrep <nowrep@gmail.com>
2011-03-03 15:26:02 +0100

    deleted tools_ folder

--- tools_/a_showLines.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/david/Programování/Qt\ C++/QupZilla/src/ && python /home/david/Programování/Qt\ C++/QupZilla/tools/pythonLineCounter.py
-read -p "Press [ENTER] to close terminal"
-exit
