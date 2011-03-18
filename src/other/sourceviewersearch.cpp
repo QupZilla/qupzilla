de215b2524406535939b8cf7a6c033b646f2589d
nowrep <nowrep@gmail.com>
2011-03-19 00:28:37 +0100

    Added search for Source Viewer

---
+++ src/other/sourceviewersearch.cpp
@@ -0,0 +1,110 @@
+/* ============================================================
+* QupZilla - WebKit based browser
+* Copyright (C) 2010-2011  nowrep
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* ============================================================ */
+#include "sourceviewersearch.h"
+#include "ui_sourceviewersearch.h"
+#include "sourceviewer.h"
+
+SourceViewerSearch::SourceViewerSearch(SourceViewer* parent) :
+    Notification((QWidget*)parent)
+   ,m_sourceViewer(parent)
+   ,ui(new Ui::SourceViewerSearch)
+{
+    setAttribute(Qt::WA_DeleteOnClose);
+    ui->setupUi(this);
+    ui->closeButton->setIcon(
+#ifdef Q_WS_X11
+    style()->standardIcon(QStyle::SP_DialogCloseButton)
+#else
+    QIcon(":/icons/faenza/close.png")
+#endif
+    );
+
+    ui->next->setIcon(
+#ifdef Q_WS_X11
+    style()->standardIcon(QStyle::SP_ArrowForward)
+#else
+    QIcon(":/icons/faenza/forward.png")
+#endif
+    );
+
+    ui->previous->setIcon(
+#ifdef Q_WS_X11
+    style()->standardIcon(QStyle::SP_ArrowBack)
+#else
+    QIcon(":/icons/faenza/back.png")
+#endif
+    );
+    ui->lineEdit->setFocus();
+    startAnimation();
+    connect(ui->closeButton, SIGNAL(clicked()), this, SLOT(hide()));
+    connect(ui->lineEdit, SIGNAL(cursorPositionChanged(int,int)), this, SLOT(next()));
+    connect(ui->next, SIGNAL(clicked()), this, SLOT(next()));
+    connect(ui->previous, SIGNAL(clicked()), this, SLOT(previous()));
+}
+
+void SourceViewerSearch::activateLineEdit()
+{
+    ui->lineEdit->setFocus();
+}
+
+void SourceViewerSearch::next()
+{
+    if (!find(0)) {
+        ui->lineEdit->setStyleSheet("QLineEdit {background:#ff6666;; }");
+        m_sourceViewer->sourceEdit()->moveCursor(QTextCursor::Start);
+    } else {
+        ui->lineEdit->setStyleSheet("");
+    }
+}
+
+void SourceViewerSearch::previous()
+{
+    if (!find(QTextDocument::FindBackward)) {
+        ui->lineEdit->setStyleSheet("QLineEdit {background:#ff6666;; }");
+        m_sourceViewer->sourceEdit()->moveCursor(QTextCursor::Start);
+    } else {
+        ui->lineEdit->setStyleSheet("");
+    }
+}
+
+bool SourceViewerSearch::find(QTextDocument::FindFlags flags)
+{
+    QString string = ui->lineEdit->text();
+    if (string.isEmpty())
+        return true;
+    if (string != m_lastSearchedString) {
+        QTextCursor cursor = m_sourceViewer->sourceEdit()->textCursor();
+        cursor.setPosition(cursor.selectionStart());
+        cursor.clearSelection();
+        m_sourceViewer->sourceEdit()->setTextCursor(cursor);
+        m_lastSearchedString = string;
+    }
+
+    if (!m_sourceViewer->sourceEdit()->find(string, flags)) {
+        QTextCursor cursor = m_sourceViewer->sourceEdit()->textCursor();
+        m_sourceViewer->sourceEdit()->moveCursor(QTextCursor::Start);
+        if (!m_sourceViewer->sourceEdit()->find(string,flags)) {
+            cursor.clearSelection();
+            m_sourceViewer->sourceEdit()->setTextCursor(cursor);
+            return false;
+        }
+    }
+    return true;
+}
+
+
