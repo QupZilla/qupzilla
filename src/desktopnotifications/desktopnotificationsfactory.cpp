6115b0bb273f08adad9f919a93c9c285c29ce477
nowrep <nowrep@gmail.com>
2011-04-27 09:05:54 +0200

    Updated copyright

--- src/desktopnotifications/desktopnotificationsfactory.cpp
+++ src/desktopnotifications/desktopnotificationsfactory.cpp
@@ -1,3 +1,20 @@
+/* ============================================================
+* QupZilla - WebKit based browser
+* Copyright (C) 2010-2011  nowrep
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* ============================================================ */
 #include "desktopnotificationsfactory.h"
 #include "desktopnotification.h"
 #include "mainapplication.h"
@@ -15,8 +32,7 @@ void DesktopNotificationsFactory::loadSettings()
     settings.beginGroup("Notifications");
     m_enabled = settings.value("Enabled", true).toBool();
     m_timeout = settings.value("Timeout", 6000).toInt();
-//#ifdef Q_WS_X11
-#if 0
+#ifdef Q_WS_X11
     m_notifType = settings.value("UseNativeDesktop", true).toBool() ? DesktopNative : PopupWidget;
 #else
     m_notifType = PopupWidget;
