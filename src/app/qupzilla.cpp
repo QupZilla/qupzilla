aff22dce7e87561b0ef13b8c8e0dab9feff5d0c6
nowrep <nowrep@gmail.com>
2011-10-02 17:42:55 +0200

    Start working on RC1

--- src/app/qupzilla.cpp
+++ src/app/qupzilla.cpp
@@ -57,7 +57,7 @@
 #include "navigationbar.h"
 #include "pagescreen.h"
 
-const QString QupZilla::VERSION = "1.0.0-b4";
+const QString QupZilla::VERSION = "1.0.0-rc1";
 //const QString QupZilla::BUILDTIME = QLocale(QLocale::English).toDateTime(__DATE__" "__TIME__, "MMM d yyyy hh:mm:ss").toString("MM/dd/yyyy hh:ss");
 const QString QupZilla::BUILDTIME =  __DATE__" "__TIME__;
 const QString QupZilla::AUTHOR = "nowrep";
