244ff31a510f62de347cd0f33731cf71254644f0
nowrep <nowrep@gmail.com>
2011-10-29 12:24:12 +0200

    Search Manager now supports also MozSearch Used for example on http://www.qtcentre.org/content/, it is working now. Also updated copyright on files where it was missing.

--- src/opensearch/opensearchreader.h
+++ src/opensearch/opensearchreader.h
@@ -16,7 +16,23 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor,
  * Boston, MA  02110-1301  USA
  */
-
+/* ============================================================
+* QupZilla - WebKit based browser
+* Copyright (C) 2010-2011  David Rosca
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* ============================================================ */
 #ifndef OPENSEARCHREADER_H
 #define OPENSEARCHREADER_H
 
@@ -33,6 +49,7 @@ public:
 
 private:
     OpenSearchEngine *read();
+    QString m_searchXml;
 
 };
 
