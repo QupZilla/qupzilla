f3970f164b536736bccf576eb63ed8d4bb1c9673
nowrep <nowrep@gmail.com>
2012-01-01 15:29:55 +0100

    Updated copyright for new year. Closes #100

--- src/app/qupzilla.cpp
+++ src/app/qupzilla.cpp
@@ -1,6 +1,6 @@
 /* ============================================================
 * QupZilla - WebKit based browser
-* Copyright (C) 2010-2011  David Rosca <nowrep@gmail.com>
+* Copyright (C) 2010-2012  David Rosca <nowrep@gmail.com>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -65,7 +65,7 @@
 const QString QupZilla::VERSION = "1.1.0";
 const QString QupZilla::BUILDTIME =  __DATE__" "__TIME__;
 const QString QupZilla::AUTHOR = "David Rosca";
-const QString QupZilla::COPYRIGHT = "2010-2011";
+const QString QupZilla::COPYRIGHT = "2010-2012";
 const QString QupZilla::WWWADDRESS = "http://qupzilla.com";
 const QString QupZilla::WIKIADDRESS = "https://github.com/nowrep/QupZilla/wiki";
 const QString QupZilla::WEBKITVERSION = qWebKitVersion();
