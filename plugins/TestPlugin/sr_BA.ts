ccdc98e8b37338c8ac3275eab3ad00e506d55605
Mladen Pejaković <pejakm@gmail.com>
2012-01-13 13:04:16 +0100

    TestPlugin Serbian translations

---
+++ plugins/TestPlugin/sr_BA.ts
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="sr_BA" sourcelanguage="en_US">
+<context>
+    <name>TestPlugin</name>
+    <message>
+        <location filename="testplugin.h" line="24"/>
+        <source>Example Plugin</source>
+        <translation>Примjер прикључaк</translation>
+    </message>
+    <message>
+        <location filename="testplugin.h" line="25"/>
+        <source>Example minimal plugin</source>
+        <translation>Примјер минималног прикључка</translation>
+    </message>
+    <message>
+        <location filename="testplugin.h" line="26"/>
+        <source>Very simple minimal plugin example</source>
+        <translation>Веома једноставан примјер прикључка</translation>
+    </message>
+    <message>
+        <location filename="testplugin.cpp" line="49"/>
+        <location filename="testplugin.cpp" line="54"/>
+        <location filename="testplugin.cpp" line="59"/>
+        <source>My first plugin action</source>
+        <translation>Моја прва радња прикључка</translation>
+    </message>
+    <message>
+        <location filename="testplugin.cpp" line="64"/>
+        <source>Hello</source>
+        <translation>Здраво</translation>
+    </message>
+    <message>
+        <location filename="testplugin.cpp" line="64"/>
+        <source>First plugin action works :-)</source>
+        <translation>Прва радња прикључка ради :-)</translation>
+    </message>
+</context>
+</TS>
