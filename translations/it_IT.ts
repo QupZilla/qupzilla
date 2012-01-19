b49488a3cbf0fe40001fe57f48e37a8c2632a1d3
Mladen Pejaković <pejakm@gmail.com>
2012-01-19 01:20:16 +0100

    Translations updates

--- translations/it_IT.ts
+++ translations/it_IT.ts
@@ -3319,6 +3319,11 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/network/qupzillaschemehandler.cpp" line="270"/>
+        <source>Max dials in row:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <location filename="../src/network/qupzillaschemehandler.cpp" line="252"/>
         <source>Load title from page</source>
         <translation>Carica titolo dalla pagina</translation>
--- translations/pt_PT.ts
+++ translations/pt_PT.ts
@@ -3316,6 +3316,11 @@ não foi encontrado!</translation>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/network/qupzillaschemehandler.cpp" line="270"/>
+        <source>Max dials in row:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <location filename="../src/network/qupzillaschemehandler.cpp" line="252"/>
         <source>Load title from page</source>
         <translation>Carregar título da página</translation>
