4fc30fca05a89c212a9d080738ca38be84551e41
nowrep <nowrep@gmail.com>
2012-01-28 13:04:14 +0100

    Edited getrevision.sh script and added one git hook - git hook to automatically update revision number after   commiting changes

--- scripts/getrevision.sh
+++ scripts/getrevision.sh
@@ -2,7 +2,9 @@
 #git show-ref refs/heads/master | cut -d " " -f 1 | cut -c 1-10
 
 cd ..
-if [ -e "git_revision" ]; then
+if [ -e "/usr/bin/git" ] &&  [ -e ".git" ]; then
+    git show-ref refs/heads/master | cut -d " " -f 1 | cut -c 1-10;
+elif [ -e "git_revision" ]; then
     cat git_revision | cut -c 1-10;
 fi
 
