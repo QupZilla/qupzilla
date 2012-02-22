QupZilla/qupzilla

commit da3d9297634c7f0e46cd9e87feb9aaa4e6459dea

Always encoding query when showing url in locationbar.

src/tools/globalfunctions.cpp
@@ -148,6 +148,15 @@ QUrl qz_makeRelativeUrl(const QUrl &baseUrl, const QUrl &rUrl)
     return returnUrl;
 }
 
+QString qz_urlEncodeQueryString(const QUrl &url)
+{
+    QString returnString = url.toString(QUrl::RemoveQuery | QUrl::RemoveFragment);
+    returnString.append(url.encodedQuery());
+    returnString.append(url.encodedFragment());
+
+    return returnString;
+}
+
 QString qz_ensureUniqueFilename(const QString &pathToFile)
 {
     if (!QFile::exists(pathToFile)) {

src/navigation/locationbar.cpp
@@ -36,6 +36,7 @@
 #include "goicon.h"
 #include "rssicon.h"
 #include "downicon.h"
+#include "globalfunctions.h"
 
 LocationBar::LocationBar(QupZilla* mainClass)
     : LineEdit()
@@ -199,7 +200,7 @@ void LocationBar::showUrl(const QUrl &url)
         return;
     }
 
-    QString stringUrl = url.toString();
+    QString stringUrl = qz_urlEncodeQueryString(url);
 
     if (stringUrl == "qupzilla:speeddial" || stringUrl == "about:blank") {
         stringUrl = "";

src/popupwindow/popuplocationbar.cpp
@@ -18,8 +18,7 @@
 #include "popuplocationbar.h"
 #include "popupwebview.h"
 #include "toolbutton.h"
-
-#include <QDebug>
+#include "globalfunctions.h"
 
 class PopupSiteIcon : public QWidget
 {
@@ -62,7 +61,7 @@ void PopupLocationBar::setView(PopupWebView* view)
 
 void PopupLocationBar::showUrl(const QUrl &url)
 {
-    setText(url.toString());
+    setText(qz_urlEncodeQueryString(url));
     setCursorPosition(0);
 }
 

src/tools/globalfunctions.h
@@ -42,6 +42,7 @@ void qz_removeDir(const QString &d);
 
 QString qz_samePartOfStrings(const QString &one, const QString &other);
 QUrl qz_makeRelativeUrl(const QUrl &baseUrl, const QUrl &rUrl);
+QString qz_urlEncodeQueryString(const QUrl &url);
 
 QString qz_ensureUniqueFilename(const QString &name);
 QString qz_getFileNameFromUrl(const QUrl &url);

commit 9a59711b78621b66bd7ac8e13e6237db321bd60b

[Fix] Fixed showing url's in locationbar. Regression from da3d929

plugins/TestPlugin/TestPlugin.pro
@@ -17,5 +17,6 @@ TRANSLATIONS += cs_CZ.ts\
                 sr_RS.ts\
                 de_DE.ts\
                 el_GR.ts\
+                id_ID.ts\
 
 include(../../plugins.pri)

src/tools/globalfunctions.cpp
@@ -151,8 +151,16 @@ QUrl qz_makeRelativeUrl(const QUrl &baseUrl, const QUrl &rUrl)
 QString qz_urlEncodeQueryString(const QUrl &url)
 {
     QString returnString = url.toString(QUrl::RemoveQuery | QUrl::RemoveFragment);
-    returnString.append(url.encodedQuery());
-    returnString.append(url.encodedFragment());
+
+    if (url.hasQuery()) {
+        returnString.append("?");
+        returnString.append(url.encodedQuery());
+    }
+
+    if (url.hasFragment()) {
+        returnString.append("#");
+        returnString.append(url.encodedFragment());
+    }
 
     return returnString;
 }

translations/homepage/id_ID.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 // Header + Footer
 $site_title = "QupZilla - Peramban multiplatform ringan";
 $qupzilla = "QupZilla";