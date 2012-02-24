ea7caa37bdcb03e59dd56d2e81244dc643c47f71
Mladen Pejaković <pejakm@gmail.com>
2012-02-24 21:58:21 +0100

    Mouse Gestures plugin ts files

---
+++ src/plugins/MouseGestures/translations/sr_BA.ts
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="sr_BA">
+<context>
+    <name>MouseGesturesSettingsDialog</name>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="14"/>
+        <source>Mouse Gestures</source>
+        <translation>Гестови миша</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="42"/>
+        <source>&lt;h1&gt;Mouse Gestures&lt;/h1&gt;</source>
+        <translation>&lt;h1&gt;Гестови миша&lt;/h1&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="98"/>
+        <source>&lt;b&gt;Stop&lt;/b&gt;&lt;br/&gt;Stop loading page</source>
+        <translation>&lt;b&gt;Заустави&lt;/b&gt;&lt;br/&gt;Заустави учитавање странице</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="112"/>
+        <source>&lt;b&gt;New tab&lt;/b&gt;&lt;br/&gt;Open new tab</source>
+        <translation>&lt;b&gt;Нови језичак&lt;/b&gt;&lt;br/&gt;Отвори нови језичак</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="126"/>
+        <source>&lt;b&gt;Back&lt;/b&gt;&lt;br/&gt;Go back in history</source>
+        <translation>&lt;b&gt;Назад&lt;/b&gt;&lt;br/&gt;Иди назад у историјату</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="140"/>
+        <source>&lt;b&gt;Forward&lt;/b&gt;&lt;br/&gt;Go forward in history</source>
+        <translation>&lt;b&gt;Напријед&lt;/b&gt;&lt;br/&gt;Иди напријед у историјату</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="160"/>
+        <source>&lt;b&gt;Reload&lt;/b&gt;&lt;br/&gt;Reload page</source>
+        <translation>&lt;b&gt;Поново учитај&lt;/b&gt;&lt;br/&gt;Поново учитај страницу</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="174"/>
+        <source>&lt;b&gt;Close tab&lt;/b&gt;&lt;br/&gt;Close current tab</source>
+        <translation>&lt;b&gt;Затвори језичак&lt;/b&gt;&lt;br/&gt;Затвори текући језичак</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="188"/>
+        <source>&lt;b&gt;Home&lt;/b&gt;&lt;br/&gt;Go to homepage</source>
+        <translation>&lt;b&gt;Домаћа&lt;/b&gt;&lt;br/&gt;Иди на домаћу страницу</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="212"/>
+        <source>License</source>
+        <translation>Лиценца</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.cpp" line="24"/>
+        <source>License Viewer</source>
+        <translation>Преглед лиценце</translation>
+    </message>
+</context>
+</TS>
