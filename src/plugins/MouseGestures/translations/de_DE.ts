a89c0861b848d635d8ce2ea0fb5b39e3f62642da
Jonathan Hooverman <Jonathan.Hooverman@gmail.com>
2012-02-25 17:10:09 +0100

    Added German translation for MouseGestures Plugin

---
+++ src/plugins/MouseGestures/translations/de_DE.ts
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de">
+<context>
+    <name>MouseGesturesSettingsDialog</name>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="14"/>
+        <source>Mouse Gestures</source>
+        <translation>Mausgesten</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="42"/>
+        <source>&lt;h1&gt;Mouse Gestures&lt;/h1&gt;</source>
+        <translation>&lt;h1&gt;Mausgesten&lt;/h1&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="64"/>
+        <source>Press and hold the middle mouse button and move your mouse in the indicated directions.</source>
+        <translation>Halten Sie die mittlere Maustaste gedrückt und bewegen Sie die Maus in die gewünschte Richtung.</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="111"/>
+        <source>&lt;b&gt;Stop&lt;/b&gt;&lt;br/&gt;Stop loading page</source>
+        <translation>&lt;b&gt;Stop&lt;/b&gt;&lt;br/&gt;Laden der Seite anhalten</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="125"/>
+        <source>&lt;b&gt;New tab&lt;/b&gt;&lt;br/&gt;Open new tab</source>
+        <translation>&lt;b&gt;Neuer Tab&lt;/b&gt;&lt;br/&gt;Neuen Tab öffnen</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="139"/>
+        <source>&lt;b&gt;Back&lt;/b&gt;&lt;br/&gt;Go back in history</source>
+        <translation>&lt;b&gt;Zurück&lt;/b&gt;&lt;br/&gt;Vorherige Seite öffnen</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="153"/>
+        <source>&lt;b&gt;Forward&lt;/b&gt;&lt;br/&gt;Go forward in history</source>
+        <translation>&lt;b&gt;Vorwärts&lt;/b&gt;&lt;br/&gt;Nächste Seite öffnen</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="173"/>
+        <source>&lt;b&gt;Reload&lt;/b&gt;&lt;br/&gt;Reload page</source>
+        <translation>&lt;b&gt;Aktualisieren&lt;/b&gt;&lt;br/&gt;Seite erneut laden</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="187"/>
+        <source>&lt;b&gt;Close tab&lt;/b&gt;&lt;br/&gt;Close current tab</source>
+        <translation>&lt;b&gt;Tab schließen&lt;/b&gt;&lt;br/&gt;Aktuellen Tab schließen</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="201"/>
+        <source>&lt;b&gt;Home&lt;/b&gt;&lt;br/&gt;Go to homepage</source>
+        <translation>&lt;b&gt;Startseite&lt;/b&gt;&lt;br/&gt;Gehe zur Startseite</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="225"/>
+        <source>License</source>
+        <translation>Lizenz</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.cpp" line="24"/>
+        <source>License Viewer</source>
+        <translation>Lizenz anzeigen</translation>
+    </message>
+</context>
+</TS>
