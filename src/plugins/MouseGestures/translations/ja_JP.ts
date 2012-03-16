4119a265fa41ba944e8b1b9a3c3dd4a8ec1f1b73
sys9kdr <sys.pdr.pdm9@gmail.com>
2012-03-16 23:07:03 +0900

    Update Japanese translations.

---
+++ src/plugins/MouseGestures/translations/ja_JP.ts
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ja_JP">
+<context>
+    <name>MouseGesturesSettingsDialog</name>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="14"/>
+        <source>Mouse Gestures</source>
+        <translation>Mouse Gestures</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="42"/>
+        <source>&lt;h1&gt;Mouse Gestures&lt;/h1&gt;</source>
+        <translation>&lt;h1&gt;Mouse Gestures&lt;/h1&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="64"/>
+        <source>Press and hold the middle mouse button and move your mouse in the indicated directions.</source>
+        <translation>マウスのホイールボタンをクリックしながら指示された方向にマウスを動かしてください。</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="111"/>
+        <source>&lt;b&gt;Stop&lt;/b&gt;&lt;br/&gt;Stop loading page</source>
+        <translation>&lt;b&gt;中止&lt;/b&gt;&lt;br/&gt;ページの読み込みを中止します</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="125"/>
+        <source>&lt;b&gt;New tab&lt;/b&gt;&lt;br/&gt;Open new tab</source>
+        <translation>&lt;b&gt;新しいタブ&lt;/b&gt;&lt;br/&gt;新しいタブを開きます</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="139"/>
+        <source>&lt;b&gt;Back&lt;/b&gt;&lt;br/&gt;Go back in history</source>
+        <translation>&lt;b&gt;戻る&lt;/b&gt;&lt;br/&gt;履歴をもとに戻ります</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="153"/>
+        <source>&lt;b&gt;Forward&lt;/b&gt;&lt;br/&gt;Go forward in history</source>
+        <translation>&lt;b&gt;進む&lt;/b&gt;&lt;br/&gt;履歴をもとに進みます</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="173"/>
+        <source>&lt;b&gt;Reload&lt;/b&gt;&lt;br/&gt;Reload page</source>
+        <translation>&lt;b&gt;更新&lt;/b&gt;&lt;br/&gt;ページを更新します</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="187"/>
+        <source>&lt;b&gt;Close tab&lt;/b&gt;&lt;br/&gt;Close current tab</source>
+        <translation>&lt;b&gt;タブを閉じる&lt;/b&gt;&lt;br/&gt;現在のタブを閉じます</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="201"/>
+        <source>&lt;b&gt;Home&lt;/b&gt;&lt;br/&gt;Go to homepage</source>
+        <translation>&lt;b&gt;ホーム&lt;/b&gt;&lt;br/&gt;ホームページへ移動します</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="225"/>
+        <source>License</source>
+        <translation>ライセンス</translation>
+    </message>
+</context>
+</TS>
