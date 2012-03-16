d5638fbc27a2913d11aed99d2049127f49a12e87
w41l <walecha99@gmail.com>
2012-03-16 12:06:57 +0700

    Updated Indonesian translations. Add new translation for plugins section.

---
+++ src/plugins/MouseGestures/translations/id_ID.ts
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="id_ID">
+<context>
+    <name>MouseGesturesSettingsDialog</name>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="14"/>
+        <source>Mouse Gestures</source>
+        <translation>Pergerakan Mouse</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="42"/>
+        <source>&lt;h1&gt;Mouse Gestures&lt;/h1&gt;</source>
+        <translation>&lt;h1&gt;Pergerakan Mouse&lt;/h1&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="64"/>
+        <source>Press and hold the middle mouse button and move your mouse in the indicated directions.</source>
+        <translation>Tekan dan tahan tombol tengah mouse dan gerakkan mouse anda ke arah yang diinginkan.</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="111"/>
+        <source>&lt;b&gt;Stop&lt;/b&gt;&lt;br/&gt;Stop loading page</source>
+        <translation>&lt;b&gt;Stop&lt;/b&gt;&lt;br/&gt;Stop memuat halaman</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="125"/>
+        <source>&lt;b&gt;New tab&lt;/b&gt;&lt;br/&gt;Open new tab</source>
+        <translation>&lt;b&gt;Tab baru&lt;/b&gt;&lt;br/&gt;Buka tab baru</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="139"/>
+        <source>&lt;b&gt;Back&lt;/b&gt;&lt;br/&gt;Go back in history</source>
+        <translation>&lt;b&gt;Mundur&lt;/b&gt;&lt;br/&gt;Mundur ke sejarah sebelumnya</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="153"/>
+        <source>&lt;b&gt;Forward&lt;/b&gt;&lt;br/&gt;Go forward in history</source>
+        <translation>&lt;b&gt;Maju&lt;/b&gt;&lt;br/&gt;Maju ke sejarah selanjutnya</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="173"/>
+        <source>&lt;b&gt;Reload&lt;/b&gt;&lt;br/&gt;Reload page</source>
+        <translation>&lt;b&gt;Muat Ulang&lt;/b&gt;&lt;br/&gt;Muat ulang halaman</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="187"/>
+        <source>&lt;b&gt;Close tab&lt;/b&gt;&lt;br/&gt;Close current tab</source>
+        <translation>&lt;b&gt;Tutup tab&lt;/b&gt;&lt;br/&gt;Tutup tab saat ini</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="201"/>
+        <source>&lt;b&gt;Home&lt;/b&gt;&lt;br/&gt;Go to homepage</source>
+        <translation>&lt;b&gt;Rumah&lt;/b&gt;&lt;br/&gt;Pergi ke halaman rumah</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="225"/>
+        <source>License</source>
+        <translation>Lisensi</translation>
+    </message>
+</context>
+</TS>
