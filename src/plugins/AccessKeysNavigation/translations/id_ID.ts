2ecb8a46811a61d5254fe6d245f85869f587dc86
w41l <walecha99@gmail.com>
2012-03-16 12:06:57 +0700

    Updated Indonesian translations. Add new translation for plugins section.

---
+++ src/plugins/AccessKeysNavigation/translations/id_ID.ts
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="id_ID">
+<context>
+    <name>AKN_Settings</name>
+    <message>
+        <location filename="../akn_settings.ui" line="14"/>
+        <source>Access Keys Navigation</source>
+        <translation>Navigasi Tombol Akses</translation>
+    </message>
+    <message>
+        <location filename="../akn_settings.ui" line="20"/>
+        <source>&lt;h1&gt;Access Keys Navigation&lt;/h1&gt;</source>
+        <translation>&lt;h1&gt;Navigasi Tombol Akses&lt;/h1&gt;</translation>
+    </message>
+    <message>
+        <location filename="../akn_settings.ui" line="33"/>
+        <source>Ctrl</source>
+        <translation>Ctrl</translation>
+    </message>
+    <message>
+        <location filename="../akn_settings.ui" line="38"/>
+        <source>Alt</source>
+        <translation>Alt</translation>
+    </message>
+    <message>
+        <location filename="../akn_settings.ui" line="43"/>
+        <source>Shift</source>
+        <translation>Shift</translation>
+    </message>
+    <message>
+        <location filename="../akn_settings.ui" line="51"/>
+        <source>Double press</source>
+        <translation>Tekan dobel</translation>
+    </message>
+    <message>
+        <location filename="../akn_settings.ui" line="58"/>
+        <source>Key for showing access keys:</source>
+        <translation>Tombol untuk menampilkan tombol akses:</translation>
+    </message>
+    <message>
+        <location filename="../akn_settings.ui" line="103"/>
+        <source>License</source>
+        <translation>Lisensi</translation>
+    </message>
+</context>
+</TS>
