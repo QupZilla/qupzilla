1044e6646339d5256f782fa766692abc142786a5
vasitsiv <vasitsiv.dev@gmail.com>
2012-03-25 20:54:27 +0300

    V2 Updated Greek Translation

---
+++ src/plugins/MouseGestures/translations/el_GR.ts
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="el">
+<context>
+    <name>MouseGesturesSettingsDialog</name>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="14"/>
+        <source>Mouse Gestures</source>
+        <translation>Χειρονομίες ποντικιού</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="42"/>
+        <source>&lt;h1&gt;Mouse Gestures&lt;/h1&gt;</source>
+        <translation>&lt;h1&gt;Χειρονομίες ποντικιού&lt;/h1&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="64"/>
+        <source>Press and hold the middle mouse button and move your mouse in the indicated directions.</source>
+        <translation>Πιέστε και κρατήστε το μεσαίο πλήκτρο του ποντικιού και μετακινήστε το ποντίκι σας στις υποδεικνυόμενες κατευθύνσεις.</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="111"/>
+        <source>&lt;b&gt;Stop&lt;/b&gt;&lt;br/&gt;Stop loading page</source>
+        <translation>&lt;b&gt;Διακοπή&lt;/b&gt;&lt;br/&gt;Διακοπή φόρτωσης σελίδας</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="125"/>
+        <source>&lt;b&gt;New tab&lt;/b&gt;&lt;br/&gt;Open new tab</source>
+        <translation>&lt;b&gt;Νέα καρτέλα&lt;/b&gt;&lt;br/&gt;Άνοιγμα νέας καρτέλας</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="139"/>
+        <source>&lt;b&gt;Back&lt;/b&gt;&lt;br/&gt;Go back in history</source>
+        <translation>&lt;b&gt;Πίσω&lt;/b&gt;&lt;br/&gt;Μετάβαση πίσω στο ιστορικό</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="153"/>
+        <source>&lt;b&gt;Forward&lt;/b&gt;&lt;br/&gt;Go forward in history</source>
+        <translation>&lt;b&gt;Μπροστά&lt;/b&gt;&lt;br/&gt;Μετάβαση μπροστά στο ιστορικό</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="173"/>
+        <source>&lt;b&gt;Reload&lt;/b&gt;&lt;br/&gt;Reload page</source>
+        <translation>&lt;b&gt;Ανανέωση&lt;/b&gt;&lt;br/&gt;Ανανέωση σελίδας</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="187"/>
+        <source>&lt;b&gt;Close tab&lt;/b&gt;&lt;br/&gt;Close current tab</source>
+        <translation>&lt;b&gt;Κλείσιμο καρτέλας&lt;/b&gt;&lt;br/&gt;Κλείσιμο τρέχουσας καρτέλας</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="201"/>
+        <source>&lt;b&gt;Home&lt;/b&gt;&lt;br/&gt;Go to homepage</source>
+        <translation>&lt;b&gt;Αρχική&lt;/b&gt;&lt;br/&gt;Μετάβαση στην αρχική σελίδα</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="225"/>
+        <source>License</source>
+        <translation>Άδεια</translation>
+    </message>
+</context>
+</TS>
