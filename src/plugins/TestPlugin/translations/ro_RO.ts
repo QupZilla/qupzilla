939bab1c14cc6fdd3997c9f464745839f6a98b2d
Stefan Comanescu <sdfanq@gmail.com>
2012-03-25 18:28:03 +0300

    Romanian translation for plugins

---
+++ src/plugins/TestPlugin/translations/ro_RO.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ro_RO" sourcelanguage="en_US">
+<context>
+    <name>TestPlugin</name>
+    <message>
+        <location filename="../testplugin.cpp" line="92"/>
+        <source>Close</source>
+        <translation>Închide</translation>
+    </message>
+    <message>
+        <location filename="../testplugin.cpp" line="103"/>
+        <source>Example Plugin Settings</source>
+        <translation>Setările plugin-ului exemplu</translation>
+    </message>
+    <message>
+        <location filename="../testplugin.cpp" line="127"/>
+        <source>My first plugin action</source>
+        <translation>Prima mea acțiune a plugin-ului</translation>
+    </message>
+    <message>
+        <location filename="../testplugin.cpp" line="139"/>
+        <source>Hello</source>
+        <translation>Salut</translation>
+    </message>
+    <message>
+        <location filename="../testplugin.cpp" line="139"/>
+        <source>First plugin action works :-)</source>
+        <translation>Prima acțiune a plugin-ului funcționează :-)</translation>
+    </message>
+</context>
+</TS>
