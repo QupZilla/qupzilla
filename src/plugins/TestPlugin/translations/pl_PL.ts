e460d9dc97422d044bf8bbe718013e7b33cecdc0
Mariusz Fik <fisiu@opensuse.org>
2012-06-29 13:28:49 +0200

    polish translation for plugins
    Signed-off-by: Mariusz Fik <fisiu@opensuse.org>

---
+++ src/plugins/TestPlugin/translations/pl_PL.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pl" sourcelanguage="en_US">
+<context>
+    <name>TestPlugin</name>
+    <message>
+        <location filename="../testplugin.cpp" line="112"/>
+        <source>Close</source>
+        <translation>Zamknij</translation>
+    </message>
+    <message>
+        <location filename="../testplugin.cpp" line="123"/>
+        <source>Example Plugin Settings</source>
+        <translation>Ustawienia przykładowej wtyczki</translation>
+    </message>
+    <message>
+        <location filename="../testplugin.cpp" line="149"/>
+        <source>My first plugin action</source>
+        <translation>Pierwsza akcja wtyczki</translation>
+    </message>
+    <message>
+        <location filename="../testplugin.cpp" line="164"/>
+        <source>Hello</source>
+        <translation>Witaj</translation>
+    </message>
+    <message>
+        <location filename="../testplugin.cpp" line="164"/>
+        <source>First plugin action works :-)</source>
+        <translation>Pierwsza akcja wtyczki działa :-)</translation>
+    </message>
+</context>
+<context>
+    <name>TestPlugin_Sidebar</name>
+    <message>
+        <location filename="../testplugin_sidebar.cpp" line="32"/>
+        <location filename="../testplugin_sidebar.cpp" line="37"/>
+        <source>Testing Sidebar</source>
+        <translation>Testowy pasek boczny</translation>
+    </message>
+</context>
+</TS>
