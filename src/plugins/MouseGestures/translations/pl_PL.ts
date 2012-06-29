4ff97abfd99d39a28fd1cb5313ff4169a4aaa3ed
Mariusz Fik <fisiu@opensuse.org>
2012-06-29 13:28:49 +0200

    polish translation for plugins
    Signed-off-by: Mariusz Fik <fisiu@opensuse.org>

---
+++ src/plugins/MouseGestures/translations/pl_PL.ts
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pl">
+<context>
+    <name>MouseGesturesSettingsDialog</name>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="14"/>
+        <source>Mouse Gestures</source>
+        <translation>Gesty myszy</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="42"/>
+        <source>&lt;h1&gt;Mouse Gestures&lt;/h1&gt;</source>
+        <translation>&lt;h1&gt;Gesty myszy&lt;/h1&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="64"/>
+        <source>Press and hold the middle mouse button and move your mouse in the indicated directions.</source>
+        <translation>Naciśnij i przytrzymaj środkowy przycisk myszy a następnie przesuń mysz we wskazanym kierunku.</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="111"/>
+        <source>&lt;b&gt;Stop&lt;/b&gt;&lt;br/&gt;Stop loading page</source>
+        <translation>&lt;b&gt;Zatrzymaj&lt;/b&gt;&lt;br/&gt;Zatrzymuje ładowanie strony</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="125"/>
+        <source>&lt;b&gt;New tab&lt;/b&gt;&lt;br/&gt;Open new tab</source>
+        <translation>&lt;b&gt;Nowa karta&lt;/b&gt;&lt;br/&gt;Otwiera nową kartę</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="139"/>
+        <source>&lt;b&gt;Back&lt;/b&gt;&lt;br/&gt;Go back in history</source>
+        <translation>&lt;b&gt;Wstecz&lt;/b&gt;&lt;br/&gt;Przechodzi do poprzedniej strony w historii</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="153"/>
+        <source>&lt;b&gt;Forward&lt;/b&gt;&lt;br/&gt;Go forward in history</source>
+        <translation>&lt;b&gt;Dalej&lt;/b&gt;&lt;br/&gt;Przechodzi do następnej strony w historii</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="173"/>
+        <source>&lt;b&gt;Reload&lt;/b&gt;&lt;br/&gt;Reload page</source>
+        <translation>&lt;b&gt;Odśwież&lt;/b&gt;&lt;br/&gt;Odświeża stronę</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="187"/>
+        <source>&lt;b&gt;Close tab&lt;/b&gt;&lt;br/&gt;Close current tab</source>
+        <translation>&lt;b&gt;Zamknij kartę&lt;/b&gt;&lt;br/&gt;Zamyka aktywną kartę</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="201"/>
+        <source>&lt;b&gt;Home&lt;/b&gt;&lt;br/&gt;Go to homepage</source>
+        <translation>&lt;b&gt;Strona domowa&lt;/b&gt;&lt;br/&gt;Przchodzi do strony domowej</translation>
+    </message>
+    <message>
+        <location filename="../mousegesturessettingsdialog.ui" line="225"/>
+        <source>License</source>
+        <translation>Licencja</translation>
+    </message>
+</context>
+</TS>
