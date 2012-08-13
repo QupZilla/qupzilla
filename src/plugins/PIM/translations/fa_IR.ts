1fea4cdbc3d3ea4bd890fdcc92e9cd3d2831fc2c
S. Razi Alavizadeh <srazi61@yahoo.com>
2012-08-14 00:10:12 +0430

    Persian translation of plugins and linux desktop file was added.

---
+++ src/plugins/PIM/translations/fa_IR.ts
@@ -0,0 +1,180 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fa" sourcelanguage="en_US">
+<context>
+    <name>PIM_Handler</name>
+    <message>
+        <location filename="../PIM_handler.cpp" line="60"/>
+        <source>First Name</source>
+        <translation>نام</translation>
+    </message>
+    <message>
+        <location filename="../PIM_handler.cpp" line="59"/>
+        <source>Last Name</source>
+        <translation>نام خانوادگی</translation>
+    </message>
+    <message>
+        <location filename="../PIM_handler.cpp" line="61"/>
+        <source>E-mail</source>
+        <translation>رایانامه</translation>
+    </message>
+    <message>
+        <location filename="../PIM_handler.cpp" line="63"/>
+        <source>Phone</source>
+        <translation>تلفن</translation>
+    </message>
+    <message>
+        <location filename="../PIM_handler.cpp" line="62"/>
+        <source>Mobile</source>
+        <translation>تلفن همراه</translation>
+    </message>
+    <message>
+        <location filename="../PIM_handler.cpp" line="64"/>
+        <source>Address</source>
+        <translation>آدرس</translation>
+    </message>
+    <message>
+        <location filename="../PIM_handler.cpp" line="65"/>
+        <source>City</source>
+        <translation>شهر</translation>
+    </message>
+    <message>
+        <location filename="../PIM_handler.cpp" line="66"/>
+        <source>ZIP Code</source>
+        <translation>کدپستی</translation>
+    </message>
+    <message>
+        <location filename="../PIM_handler.cpp" line="67"/>
+        <source>State/Region</source>
+        <translation>ناحیه/ایالت</translation>
+    </message>
+    <message>
+        <location filename="../PIM_handler.cpp" line="68"/>
+        <source>Country</source>
+        <translation>کشور</translation>
+    </message>
+    <message>
+        <location filename="../PIM_handler.cpp" line="69"/>
+        <source>Home Page</source>
+        <translation>صفحه خانگی</translation>
+    </message>
+    <message>
+        <location filename="../PIM_handler.cpp" line="70"/>
+        <source>Custom 1</source>
+        <translation>شخصی 1</translation>
+    </message>
+    <message>
+        <location filename="../PIM_handler.cpp" line="71"/>
+        <source>Custom 2</source>
+        <translation>شخصی 2</translation>
+    </message>
+    <message>
+        <location filename="../PIM_handler.cpp" line="72"/>
+        <source>Custom 3</source>
+        <translation>شخصی32</translation>
+    </message>
+    <message>
+        <location filename="../PIM_handler.cpp" line="110"/>
+        <source>Insert Personal Information</source>
+        <translation>اطلاعات شخصی را وارد کن</translation>
+    </message>
+    <message>
+        <location filename="../PIM_handler.cpp" line="124"/>
+        <source>Edit</source>
+        <translation>ویرایش</translation>
+    </message>
+</context>
+<context>
+    <name>PIM_Settings</name>
+    <message>
+        <location filename="../PIM_settings.ui" line="14"/>
+        <source>PIM Settings</source>
+        <translation>سامانش PIM</translation>
+    </message>
+    <message>
+        <location filename="../PIM_settings.ui" line="76"/>
+        <source>First Name:</source>
+        <translation>نام:</translation>
+    </message>
+    <message>
+        <location filename="../PIM_settings.ui" line="89"/>
+        <source>Last Name:</source>
+        <translation>نام خانوادگی:</translation>
+    </message>
+    <message>
+        <location filename="../PIM_settings.ui" line="115"/>
+        <source>Phone:</source>
+        <translation>تلفن:</translation>
+    </message>
+    <message>
+        <location filename="../PIM_settings.ui" line="128"/>
+        <source>Mobile Phone:</source>
+        <translation>تلفن همراه:</translation>
+    </message>
+    <message>
+        <location filename="../PIM_settings.ui" line="141"/>
+        <source>Address:</source>
+        <translation>آدرس:</translation>
+    </message>
+    <message>
+        <location filename="../PIM_settings.ui" line="42"/>
+        <source>&lt;h2&gt;Personal Information Manager&lt;/h2&gt;</source>
+        <translation>&lt;h2&gt;مدیر اطلاغات شخصی&lt;/h2&gt;</translation>
+    </message>
+    <message>
+        <location filename="../PIM_settings.ui" line="64"/>
+        <source>Your personal information that will be used on webpages.</source>
+        <translation>اطلاعات شخصی شما که بر روی صفحه‌های وب استفاده خواهند شد.</translation>
+    </message>
+    <message>
+        <location filename="../PIM_settings.ui" line="154"/>
+        <source>City:</source>
+        <translation>شهر:</translation>
+    </message>
+    <message>
+        <location filename="../PIM_settings.ui" line="102"/>
+        <source>E-mail:</source>
+        <translation>رایانامه:</translation>
+    </message>
+    <message>
+        <location filename="../PIM_settings.ui" line="167"/>
+        <source>ZIP Code:</source>
+        <translation>کدپستی:</translation>
+    </message>
+    <message>
+        <location filename="../PIM_settings.ui" line="180"/>
+        <source>State/Region:</source>
+        <translation>ناحیه/ایالت:</translation>
+    </message>
+    <message>
+        <location filename="../PIM_settings.ui" line="193"/>
+        <source>Country:</source>
+        <translation>کشور:</translation>
+    </message>
+    <message>
+        <location filename="../PIM_settings.ui" line="206"/>
+        <source>Home Page:</source>
+        <translation>صفحه خانگی:</translation>
+    </message>
+    <message>
+        <location filename="../PIM_settings.ui" line="219"/>
+        <source>Custom 1:</source>
+        <translation>شخصی 1:</translation>
+    </message>
+    <message>
+        <location filename="../PIM_settings.ui" line="232"/>
+        <source>Custom 2:</source>
+        <translation>شخصی 2:</translation>
+    </message>
+    <message>
+        <location filename="../PIM_settings.ui" line="245"/>
+        <source>Custom 3:</source>
+        <translation>شخصی 3:</translation>
+    </message>
+    <message>
+        <location filename="../PIM_settings.ui" line="260"/>
+        <source>&lt;b&gt;Note:&lt;/b&gt; Press Ctrl+ENTER to autofill form fields for which QupZilla finds personal entries.</source>
+        <translation>&lt;b&gt;توجه:&lt;/b&gt; برای پرکردن خودکار فیلدهای فرم Ctrl+Enter را بفشارید تا کوپزیلا ورودی‌های شخصی‌تان را پیدا کند.</translation>
+    </message>
+</context>
+</TS>
