QupZilla/qupzilla

commit 7a1bbd993d6f54d01b88a8d1f05bcfe0dec1c2b5

Fixed infinite loop when adding a bookmark
- when history is empty
Closes #652

src/lib/history/historymodel.cpp
@@ -446,7 +446,7 @@ void HistoryModel::init()
 {
     QSqlQuery query;
     query.exec("SELECT MIN(date) FROM history");
-    if (!query.next()) {
+    if (query.size() <= 0) {
         return;
     }
 

commit 454d0d47ddeffbafe971250dc33e04a17cf03fba

[Fix] Infinite loop on bookmarking a page while history is empty
Previous fix as in 7a1bbd9 introduced new bug with history
sidebar being always empty.
This commit fixes this bug.

src/lib/history/historymodel.cpp
@@ -446,15 +446,18 @@ void HistoryModel::init()
 {
     QSqlQuery query;
     query.exec("SELECT MIN(date) FROM history");
-    if (query.size() <= 0) {
+    if (!query.next()) {
+        return;
+    }
+
+    const qint64 &minTimestamp = query.value(0).toLongLong();
+    if (minTimestamp <= 0) {
         return;
     }
 
     const QDate &today = QDate::currentDate();
     const QDate &week = today.addDays(1 - today.dayOfWeek());
     const QDate &month = QDate(today.year(), today.month(), 1);
-
-    const qint64 &minTimestamp = query.value(0).toLongLong();
     const qint64 &currentTimestamp = QDateTime::currentMSecsSinceEpoch();
 
     qint64 timestamp = currentTimestamp;