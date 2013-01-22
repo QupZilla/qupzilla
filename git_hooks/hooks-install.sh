a71302fbc6f91aaf75ac8afca7f62227ea5ef767
nowrep <nowrep@gmail.com>
2013-01-22 23:30:59 +0100

    [git-hooks] New hook for checking copyright year.

--- git_hooks/hooks-install.sh
+++ git_hooks/hooks-install.sh
@@ -3,6 +3,7 @@
 # Install hooks into .git/hooks directory
 #
 
+cp pre-commit ../.git/hooks/pre-commit
 cp post-commit ../.git/hooks/post-commit
 cp post-checkout ../.git/hooks/post-checkout
 cp post-merge ../.git/hooks/post-merge
