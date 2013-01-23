QupZilla/qupzilla

commit e28f9d438e4253a432ae57752a4cb87c682987ae

[Code] qz_ prefixed functions moved to QzTools namespace.

src/lib/plugins/clicktoflash.cpp
@@ -43,7 +43,7 @@
 #include "pluginproxy.h"
 #include "squeezelabelv2.h"
 #include "webpage.h"
-#include "globalfunctions.h"
+#include "qztools.h"
 #include "qupzilla.h"
 
 #include <QHBoxLayout>
@@ -228,17 +228,17 @@ void ClickToFlash::load()
            So asynchronous JavaScript code is used to remove element from page and then substitute
            it with unblocked Flash. The JavaScript code is:
 
-               var qz_c2f_clone = this.cloneNode(true);
-               var qz_c2f_parentNode = this.parentNode;
-               var qz_c2f_substituteElement = document.createElement(this.tagName);
+               var QzTools::c2f_clone = this.cloneNode(true);
+               var QzTools::c2f_parentNode = this.parentNode;
+               var QzTools::c2f_substituteElement = document.createElement(this.tagName);
 
-               qz_c2f_substituteElement.width = this.width;
-               qz_c2f_substituteElement.height = this.height;
+               QzTools::c2f_substituteElement.width = this.width;
+               QzTools::c2f_substituteElement.height = this.height;
 
-               this.parentNode.replaceChild(qz_c2f_substituteElement, this);
+               this.parentNode.replaceChild(QzTools::c2f_substituteElement, this);
 
                setTimeout(function(){
-                   qz_c2f_parentNode.replaceChild(qz_c2f_clone, qz_c2f_substituteElement);
+                   QzTools::c2f_parentNode.replaceChild(QzTools::c2f_clone, QzTools::c2f_substituteElement);
                }, 250);
 
         */
@@ -249,11 +249,11 @@ void ClickToFlash::load()
 
         m_element.setAttribute("type", "application/futuresplash");
 
-        QString js = "var qz_c2f_clone=this.cloneNode(true);var qz_c2f_parentNode=this.parentNode;"
-                     "var qz_c2f_substituteElement=document.createElement(this.tagName);"
-                     "qz_c2f_substituteElement.width=this.width;qz_c2f_substituteElement.height=this.height;"
-                     "this.parentNode.replaceChild(qz_c2f_substituteElement,this);setTimeout(function(){"
-                     "qz_c2f_parentNode.replaceChild(qz_c2f_clone,qz_c2f_substituteElement);},250);";
+        QString js = "var QzTools::c2f_clone=this.cloneNode(true);var QzTools::c2f_parentNode=this.parentNode;"
+                     "var QzTools::c2f_substituteElement=document.createElement(this.tagName);"
+                     "QzTools::c2f_substituteElement.width=this.width;QzTools::c2f_substituteElement.height=this.height;"
+                     "this.parentNode.replaceChild(QzTools::c2f_substituteElement,this);setTimeout(function(){"
+                     "QzTools::c2f_parentNode.replaceChild(QzTools::c2f_clone,QzTools::c2f_substituteElement);},250);";
 
         m_element.evaluateJavaScript(js);
     }
@@ -312,6 +312,6 @@ void ClickToFlash::showInfo()
     }
 
     widg->setMaximumHeight(500);
-    qz_centerWidgetToParent(widg, m_page->view());
+    QzTools::centerWidgetToParent(widg, m_page->view());
     widg->show();
 }

src/lib/adblock/adblockmanager.cpp
@@ -21,7 +21,7 @@
 #include "adblockblockednetworkreply.h"
 #include "mainapplication.h"
 #include "webpage.h"
-#include "globalfunctions.h"
+#include "qztools.h"
 #include "networkmanager.h"
 #include "qupzilla.h"
 #include "settings.h"
@@ -128,8 +128,8 @@ AdBlockSubscription* AdBlockManager::addSubscription(const QString &title, const
         return 0;
     }
 
-    QString fileName = qz_filterCharsFromFilename(title.toLower()) + ".txt";
-    QString filePath = qz_ensureUniqueFilename(mApp->currentProfilePath() + "adblock/" + fileName);
+    QString fileName = QzTools::filterCharsFromFilename(title.toLower()) + ".txt";
+    QString filePath = QzTools::ensureUniqueFilename(mApp->currentProfilePath() + "adblock/" + fileName);
 
     QByteArray data = QString("Title: %1\nUrl: %2\n[Adblock Plus 1.1.1]").arg(title, url).toLatin1();
 

src/lib/adblock/adblocksubscription.cpp
@@ -46,7 +46,7 @@
 #include "adblockmanager.h"
 #include "mainapplication.h"
 #include "networkmanager.h"
-#include "globalfunctions.h"
+#include "qztools.h"
 #include "followredirectreply.h"
 
 #include <QFile>
@@ -257,7 +257,7 @@ QString AdBlockSubscription::elementHidingRulesForDomain(const QString &domain) 
 
 const AdBlockRule* AdBlockSubscription::rule(int offset) const
 {
-    if (!qz_listContainsIndex(m_rules, offset)) {
+    if (!QzTools::listContainsIndex(m_rules, offset)) {
         return 0;
     }
 
@@ -271,7 +271,7 @@ QList<AdBlockRule> AdBlockSubscription::allRules() const
 
 const AdBlockRule* AdBlockSubscription::enableRule(int offset)
 {
-    if (!qz_listContainsIndex(m_rules, offset)) {
+    if (!QzTools::listContainsIndex(m_rules, offset)) {
         return 0;
     }
 
@@ -289,7 +289,7 @@ const AdBlockRule* AdBlockSubscription::enableRule(int offset)
 
 const AdBlockRule* AdBlockSubscription::disableRule(int offset)
 {
-    if (!qz_listContainsIndex(m_rules, offset)) {
+    if (!QzTools::listContainsIndex(m_rules, offset)) {
         return 0;
     }
 
@@ -479,7 +479,7 @@ int AdBlockCustomList::addRule(const AdBlockRule &rule)
 
 bool AdBlockCustomList::removeRule(int offset)
 {
-    if (!qz_listContainsIndex(m_rules, offset)) {
+    if (!QzTools::listContainsIndex(m_rules, offset)) {
         return false;
     }
 
@@ -497,7 +497,7 @@ bool AdBlockCustomList::removeRule(int offset)
 
 const AdBlockRule* AdBlockCustomList::replaceRule(const AdBlockRule &rule, int offset)
 {
-    if (!qz_listContainsIndex(m_rules, offset)) {
+    if (!QzTools::listContainsIndex(m_rules, offset)) {
         return 0;
     }
 

src/lib/app/mainapplication.cpp
@@ -37,7 +37,7 @@
 #include "qtwin.h"
 #include "mainapplication.h"
 #include "webhistoryinterface.h"
-#include "globalfunctions.h"
+#include "qztools.h"
 #include "profileupdater.h"
 #include "searchenginesmanager.h"
 #include "databasewriter.h"
@@ -1057,7 +1057,7 @@ void MainApplication::clearTempPath()
     QDir dir(path);
 
     if (dir.exists()) {
-        qz_removeDir(path);
+        QzTools::removeDir(path);
     }
 }
 

src/lib/app/profileupdater.cpp
@@ -18,7 +18,7 @@
 #include "profileupdater.h"
 #include "qupzilla.h"
 #include "updater.h"
-#include "globalfunctions.h"
+#include "qztools.h"
 #include "mainapplication.h"
 
 #include <QDir>
@@ -112,8 +112,8 @@ void ProfileUpdater::copyDataToProfile()
 
     QFile browseData(m_profilePath + "browsedata.db");
     if (browseData.exists()) {
-        const QString &browseDataBackup = qz_ensureUniqueFilename(m_profilePath + "browsedata-backup.db");
-        const QString &settingsBackup = qz_ensureUniqueFilename(m_profilePath + "settings-backup.ini");
+        const QString &browseDataBackup = QzTools::ensureUniqueFilename(m_profilePath + "browsedata-backup.db");
+        const QString &settingsBackup = QzTools::ensureUniqueFilename(m_profilePath + "settings-backup.ini");
         browseData.copy(browseDataBackup);
         QFile(m_profilePath + "settings.ini").copy(settingsBackup);
         const QString &text = "Incompatible profile version has been detected. To avoid losing your profile data, they were "

src/lib/app/qupzilla.cpp
@@ -56,7 +56,7 @@
 #include "pagescreen.h"
 #include "webinspectordockwidget.h"
 #include "bookmarksimportdialog.h"
-#include "globalfunctions.h"
+#include "qztools.h"
 #include "reloadstopbutton.h"
 #include "enhancedmenu.h"
 #include "settings.h"
@@ -1286,7 +1286,7 @@ void QupZilla::showSource(QWebFrame* frame, const QString &selectedHtml)
     }
 
     SourceViewer* source = new SourceViewer(frame, selectedHtml);
-    qz_centerWidgetToParent(source, this);
+    QzTools::centerWidgetToParent(source, this);
     source->show();
 }
 
@@ -1953,7 +1953,7 @@ bool QupZilla::restoreState(const QByteArray &state, int version)
 #ifdef QZ_WS_X11
 int QupZilla::getCurrentVirtualDesktop() const
 {
-    Display* display = static_cast<Display*>(qz_X11Display(this));
+    Display* display = static_cast<Display*>(QzTools::X11Display(this));
     Atom actual_type;
     int actual_format;
     unsigned long nitems;
@@ -1986,7 +1986,7 @@ void QupZilla::moveToVirtualDesktop(int desktopId)
         return;
     }
 
-    Display* display = static_cast<Display*>(qz_X11Display(this));
+    Display* display = static_cast<Display*>(QzTools::X11Display(this));
 
     Atom net_wm_desktop = XInternAtom(display, "_NET_WM_DESKTOP", False);
     if (net_wm_desktop == None) {

src/lib/bookmarks/bookmarksmanager.cpp
@@ -25,7 +25,7 @@
 #include "bookmarksmodel.h"
 #include "iconprovider.h"
 #include "browsinglibrary.h"
-#include "globalfunctions.h"
+#include "qztools.h"
 #include "bookmarksimportdialog.h"
 #include "iconchooser.h"
 #include "webtab.h"

src/lib/bookmarksimport/chromeimporter.cpp
@@ -16,7 +16,7 @@
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 * ============================================================ */
 #include "chromeimporter.h"
-#include "globalfunctions.h"
+#include "qztools.h"
 #include "bookmarksimportdialog.h"
 
 #include <QScriptEngine>

commit 25cf1eedc4538b65015b6d3f9dce3a4bc8fdac6d

Fixed broken Click2Flash from previous commit.
Broken by mistake in e28f9d438e4253a432ae57752a4cb87c682987ae

src/lib/autofill/autofillmodel.cpp
@@ -261,7 +261,7 @@ void AutoFillModel::completePage(WebPage* page)
         }
     }
 }
-#include <QDebug>
+
 void AutoFillModel::post(const QNetworkRequest &request, const QByteArray &outgoingData)
 {
     // Don't save in private browsing

src/lib/plugins/clicktoflash.cpp
@@ -1,6 +1,6 @@
 /* ============================================================
 * QupZilla - WebKit based browser
-* Copyright (C) 2010-2012  David Rosca <nowrep@gmail.com>
+* Copyright (C) 2010-2013  David Rosca <nowrep@gmail.com>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -219,7 +219,7 @@ void ClickToFlash::load()
     else {
         /*
            Old code caused sometimes flashing of the whole browser window and then somehow
-           ruined rendering of opacity effects, etc.. in the window on X11.
+           ruined rendering of opacity effects, etc..
 
                 QWebElement substitute = m_element.clone();
                 substitute.setAttribute(QLatin1String("type"), "application/futuresplash");
@@ -228,18 +228,18 @@ void ClickToFlash::load()
            So asynchronous JavaScript code is used to remove element from page and then substitute
            it with unblocked Flash. The JavaScript code is:
 
-               var QzTools::c2f_clone = this.cloneNode(true);
-               var QzTools::c2f_parentNode = this.parentNode;
-               var QzTools::c2f_substituteElement = document.createElement(this.tagName);
+                var qz_c2f_clone = this.cloneNode(true);
+                var qz_c2f_parentNode = this.parentNode;
+                var qz_c2f_substituteElement = document.createElement(this.tagName);
 
-               QzTools::c2f_substituteElement.width = this.width;
-               QzTools::c2f_substituteElement.height = this.height;
+                qz_c2f_substituteElement.width = this.width;
+                qz_c2f_substituteElement.height = this.height;
 
-               this.parentNode.replaceChild(QzTools::c2f_substituteElement, this);
+                this.parentNode.replaceChild(qz_c2f_substituteElement, this);
 
-               setTimeout(function(){
-                   QzTools::c2f_parentNode.replaceChild(QzTools::c2f_clone, QzTools::c2f_substituteElement);
-               }, 250);
+                setTimeout(function(){
+                    qz_c2f_parentNode.replaceChild(qz_c2f_clone, qz_c2f_substituteElement);
+                }, 250);
 
         */
 
@@ -249,11 +249,11 @@ void ClickToFlash::load()
 
         m_element.setAttribute("type", "application/futuresplash");
 
-        QString js = "var QzTools::c2f_clone=this.cloneNode(true);var QzTools::c2f_parentNode=this.parentNode;"
-                     "var QzTools::c2f_substituteElement=document.createElement(this.tagName);"
-                     "QzTools::c2f_substituteElement.width=this.width;QzTools::c2f_substituteElement.height=this.height;"
-                     "this.parentNode.replaceChild(QzTools::c2f_substituteElement,this);setTimeout(function(){"
-                     "QzTools::c2f_parentNode.replaceChild(QzTools::c2f_clone,QzTools::c2f_substituteElement);},250);";
+        QString js = "var qz_c2f_clone=this.cloneNode(true);var qz_c2f_parentNode=this.parentNode;"
+                     "var qz_c2f_substituteElement=document.createElement(this.tagName);"
+                     "qz_c2f_substituteElement.width=this.width;qz_c2f_substituteElement.height=this.height;"
+                     "this.parentNode.replaceChild(qz_c2f_substituteElement,this);setTimeout(function(){"
+                     "qz_c2f_parentNode.replaceChild(qz_c2f_clone,qz_c2f_substituteElement);},250);";
 
         m_element.evaluateJavaScript(js);
     }