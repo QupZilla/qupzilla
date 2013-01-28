780106ddd084008a08e51e8d6beac555d2b83872
nowrep <nowrep@gmail.com>
2013-01-28 19:38:03 +0100

    Draw cursor in locationbar when completion popup is shown.
    Closes #645

--- src/lib/navigation/completer/locationcompleter.cpp
+++ src/lib/navigation/completer/locationcompleter.cpp
@@ -1,6 +1,6 @@
 /* ============================================================
 * QupZilla - WebKit based browser
-* Copyright (C) 2010-2012  David Rosca <nowrep@gmail.com>
+* Copyright (C) 2010-2013  David Rosca <nowrep@gmail.com>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -41,6 +41,11 @@ LocationCompleter::LocationCompleter(QObject* parent)
 void LocationCompleter::setLocationBar(LocationBar* locationBar)
 {
     m_locationBar = locationBar;
+}
+
+bool LocationCompleter::isPopupVisible()
+{
+    return s_view->isVisible();
 }
 
 void LocationCompleter::closePopup()
