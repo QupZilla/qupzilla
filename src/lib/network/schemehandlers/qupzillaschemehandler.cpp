f0d4be4267d1b9a71df322721a6cc45877143841
Mladen Pejaković <pejakm@gmail.com>
2013-01-31 01:40:23 +0100

    Build Configuration WebGL flag fix for qtwebkit 2.3

--- src/lib/network/schemehandlers/qupzillaschemehandler.cpp
+++ src/lib/network/schemehandlers/qupzillaschemehandler.cpp
@@ -390,7 +390,7 @@ QString QupZillaSchemeReply::configPage()
 #ifdef QUPZILLA_DEBUG_BUILD
         debugBuild = tr("<b>Enabled</b>");
 #endif
-#ifdef USE_WEBGL
+#if defined (USE_WEBGL) || QTWEBKIT_FROM_2_3
         webGLEnabled = tr("<b>Enabled</b>");
 #endif
 #if defined(Q_OS_WIN) && defined(W7API)
