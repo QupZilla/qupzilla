QupZilla/qupzilla

commit 38c4d347c9910288e059fddf49791de8e1278f55

[TabsOnTop] Fixed flickering when closing tab.
It only occured when closing tab and going to tab that hasn't
been loaded yet (don't load tabs until selected)

src/lib/webview/webtab.cpp
@@ -30,6 +30,7 @@
 #include <QWebFrame>
 #include <QLabel>
 #include <QStyle>
+#include <QTimer>
 
 WebTab::SavedTab::SavedTab(WebTab* webTab)
 {
@@ -110,9 +111,7 @@ TabbedWebView* WebTab::view() const
 void WebTab::setCurrentTab()
 {
     if (!isRestored()) {
-        p_restoreTab(m_savedTab);
-
-        m_savedTab.clear();
+        QTimer::singleShot(0, this, SLOT(slotRestore()));
     }
 }
 
@@ -303,6 +302,12 @@ void WebTab::showNotification(QWidget* notif)
 
     m_layout->insertWidget(notifPos - 1, notif);
     notif->show();
+}
+
+void WebTab::slotRestore()
+{
+    p_restoreTab(m_savedTab);
+    m_savedTab.clear();
 }
 
 int WebTab::tabIndex() const

src/lib/webview/webtab.h
@@ -98,6 +98,7 @@ public:
 
 private slots:
     void showNotification(QWidget* notif);
+    void slotRestore();
 
 private:
     QupZilla* p_QupZilla;

commit 3b4d2f5534be5f26d622794120d40cf02f7457df

[Regression] Fixed restoring current tab.
Regression from 38c4d347c9910288e059fddf49791de8e1278f55

src/lib/webview/webtab.cpp
@@ -111,7 +111,12 @@ TabbedWebView* WebTab::view() const
 void WebTab::setCurrentTab()
 {
     if (!isRestored()) {
-        QTimer::singleShot(0, this, SLOT(slotRestore()));
+        if (isVisible()) {
+            QTimer::singleShot(0, this, SLOT(slotRestore()));
+        }
+        else {
+            slotRestore();
+        }
     }
 }
 