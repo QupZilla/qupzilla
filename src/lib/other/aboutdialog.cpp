498f4b055565bf814fb8f6bbef3efec61b4d801a
nowrep <nowrep@gmail.com>
2013-02-18 19:19:26 +0100

    [Windows] Adjust height of AboutDialog.

--- src/lib/other/aboutdialog.cpp
+++ src/lib/other/aboutdialog.cpp
@@ -1,6 +1,6 @@
 /* ============================================================
 * QupZilla - WebKit based browser
-* Copyright (C) 2010-2012  David Rosca <nowrep@gmail.com>
+* Copyright (C) 2010-2013  David Rosca <nowrep@gmail.com>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -39,6 +39,10 @@ AboutDialog::AboutDialog(QWidget* parent)
     connect(ui->authorsButton, SIGNAL(clicked()), this, SLOT(buttonClicked()));
 
     showAbout();
+
+#ifdef Q_OS_WIN
+    resize(300, height());
+#endif
 }
 
 void AboutDialog::buttonClicked()
