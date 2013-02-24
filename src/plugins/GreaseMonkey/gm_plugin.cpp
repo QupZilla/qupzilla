4eba746a2c59205b5d7eaed5e7ee95db772d93c5
nowrep <nowrep@gmail.com>
2013-02-24 12:07:57 +0100

    GreaseMonkey: Bumped plugin version.

--- src/plugins/GreaseMonkey/gm_plugin.cpp
+++ src/plugins/GreaseMonkey/gm_plugin.cpp
@@ -1,6 +1,6 @@
 /* ============================================================
 * GreaseMonkey plugin for QupZilla
-* Copyright (C) 2012  David Rosca <nowrep@gmail.com>
+* Copyright (C) 2012-2013  David Rosca <nowrep@gmail.com>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -38,7 +38,7 @@ PluginSpec GM_Plugin::pluginSpec()
     spec.name = "GreaseMonkey";
     spec.info = "Userscripts for QupZilla";
     spec.description = "Provides support for userscripts (www.userscripts.org)";
-    spec.version = "0.2.3";
+    spec.version = "0.2.5";
     spec.author = "David Rosca <nowrep@gmail.com>";
     spec.icon = QPixmap(":gm/data/icon.png");
     spec.hasSettings = true;
