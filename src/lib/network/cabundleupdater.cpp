e455f58aa1adb9e4857c229fe70cb5aff61ce3b6
nowrep <nowrep@gmail.com>
2013-03-11 15:47:03 +0100

    [CaBundleUpdater] Reload certificates after finished bundle update.

--- src/lib/network/cabundleupdater.cpp
+++ src/lib/network/cabundleupdater.cpp
@@ -1,6 +1,6 @@
 /* ============================================================
 * QupZilla - WebKit based browser
-* Copyright (C) 2010-2012  David Rosca <nowrep@gmail.com>
+* Copyright (C) 2010-2013  David Rosca <nowrep@gmail.com>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -123,5 +123,8 @@ void CaBundleUpdater::replyFinished()
         }
 
         file.write(response);
+
+        // Reload newly downloaded certificates
+        mApp->networkManager()->loadSettings();
     }
 }
