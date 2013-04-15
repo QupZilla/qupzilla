QupZilla/qupzilla

commit 7c9b30c4432329afadaa63022c59fe14bd3b6c75

Fixed, sent correct event for right/left key on RTL layouts.
-see #839

src/lib/webview/webview.cpp
@@ -1229,7 +1229,20 @@ void WebView::keyPressEvent(QKeyEvent* event)
         return;
     }
 
-    switch (event->key()) {
+    int eventKey = event->key();
+
+    bool rightOrLeft = eventKey == Qt::Key_Left || eventKey == Qt::Key_Right;
+    if (rightOrLeft) {
+        const QWebElement &elementHasCursor = activeElement();
+        if (!elementHasCursor.isNull()) {
+            bool isRTL =  elementHasCursor.styleProperty("direction", QWebElement::ComputedStyle) == QLatin1String("rtl");
+            if (isRTL) {
+                eventKey = eventKey == Qt::Key_Left ? Qt::Key_Right : Qt::Key_Left;
+            }
+        }
+    }
+
+    switch (eventKey) {
     case Qt::Key_C:
         if (event->modifiers() == Qt::ControlModifier) {
             triggerPageAction(QWebPage::Copy);
@@ -1318,6 +1331,8 @@ void WebView::keyPressEvent(QKeyEvent* event)
         break;
     }
 
+    event = new QKeyEvent(event->type(), eventKey, event->modifiers(),
+                          event->text(), event->isAutoRepeat());
     QWebView::keyPressEvent(event);
 }
 

commit 373050bef0c78d5f222cc74bfbb799c614db6b3b

[Regression] Fixed handling key press events in webview.
Regression from 7c9b30c4432329afadaa63022c59fe14bd3b6c75

src/lib/webview/webview.cpp
@@ -1231,13 +1231,19 @@ void WebView::keyPressEvent(QKeyEvent* event)
 
     int eventKey = event->key();
 
-    bool rightOrLeft = eventKey == Qt::Key_Left || eventKey == Qt::Key_Right;
-    if (rightOrLeft) {
+    // The right/left arrow keys within contents with right to left (RTL) layout have
+    // reversed behavior than left to right (LTR) layout.
+    // Example: Key_Right within LTR layout triggers QWebPage::MoveToNextChar but,
+    // Key_Right within RTL layout should trigger QWebPage::MoveToPreviousChar
+
+    if (eventKey == Qt::Key_Left || eventKey == Qt::Key_Right) {
         const QWebElement &elementHasCursor = activeElement();
         if (!elementHasCursor.isNull()) {
-            bool isRTL =  elementHasCursor.styleProperty("direction", QWebElement::ComputedStyle) == QLatin1String("rtl");
-            if (isRTL) {
+            const QString &direction = elementHasCursor.styleProperty("direction", QWebElement::ComputedStyle);
+            if (direction == QLatin1String("rtl")) {
                 eventKey = eventKey == Qt::Key_Left ? Qt::Key_Right : Qt::Key_Left;
+                QKeyEvent ev(event->type(), eventKey, event->modifiers(), event->text(), event->isAutoRepeat());
+                event = &ev;
             }
         }
     }
@@ -1331,8 +1337,6 @@ void WebView::keyPressEvent(QKeyEvent* event)
         break;
     }
 
-    event = new QKeyEvent(event->type(), eventKey, event->modifiers(),
-                          event->text(), event->isAutoRepeat());
     QWebView::keyPressEvent(event);
 }
 