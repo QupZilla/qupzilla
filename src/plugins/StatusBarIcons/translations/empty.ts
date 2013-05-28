a30ff3f0ba5854d675311976eb03b6c9bffd4f77
nowrep <nowrep@gmail.com>
2013-05-28 21:21:32 +0200

    [StatusBarIcons] Updated translations

--- src/plugins/StatusBarIcons/translations/empty.ts
+++ src/plugins/StatusBarIcons/translations/empty.ts
@@ -70,23 +70,163 @@
 <context>
     <name>SBI_NetworkIcon</name>
     <message>
-        <location filename="../sbi_networkicon.cpp" line="59"/>
+        <location filename="../sbi_networkicon.cpp" line="73"/>
+        <source>Proxy configuration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sbi_networkicon.cpp" line="75"/>
+        <source>Select proxy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sbi_networkicon.cpp" line="89"/>
+        <source>Empty</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sbi_networkicon.cpp" line="93"/>
+        <source>Manage proxies</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sbi_networkicon.cpp" line="106"/>
         <source>Shows network status and manages proxy&lt;br/&gt;&lt;br/&gt;&lt;b&gt;Network:&lt;/b&gt;&lt;br/&gt;%1&lt;br/&gt;&lt;br/&gt;&lt;b&gt;Proxy:&lt;/b&gt;&lt;br/&gt;%2</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../sbi_networkicon.cpp" line="63"/>
+        <location filename="../sbi_networkicon.cpp" line="110"/>
         <source>Connected</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../sbi_networkicon.cpp" line="67"/>
+        <location filename="../sbi_networkicon.cpp" line="114"/>
         <source>Offline</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../sbi_networkicon.cpp" line="71"/>
+        <location filename="../sbi_networkicon.cpp" line="118"/>
         <source>Unknown</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SBI_NetworkIconDialog</name>
+    <message>
+        <location filename="../sbi_networkicondialog.ui" line="14"/>
+        <source>Proxy Manager</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sbi_networkicondialog.ui" line="28"/>
+        <source>Select proxy: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sbi_networkicondialog.ui" line="38"/>
+        <location filename="../sbi_networkicondialog.cpp" line="54"/>
+        <source>Add proxy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sbi_networkicondialog.ui" line="45"/>
+        <source>Remove proxy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sbi_networkicondialog.ui" line="69"/>
+        <source>No proxies. You can add proxy by clicking on &lt;b&gt;Add&lt;/b&gt; button.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sbi_networkicondialog.ui" line="104"/>
+        <source>All changes must be saved with &lt;b&gt;Save&lt;/b&gt; button.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sbi_networkicondialog.cpp" line="54"/>
+        <source>Name of proxy:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SBI_ProxyWidget</name>
+    <message>
+        <location filename="../sbi_proxywidget.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sbi_proxywidget.ui" line="20"/>
+        <source>Do not use proxy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sbi_proxywidget.ui" line="27"/>
+        <source>Use different proxy for https connection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sbi_proxywidget.ui" line="34"/>
+        <source>&lt;b&gt;Exceptions&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sbi_proxywidget.ui" line="43"/>
+        <source>Don&apos;t use on:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sbi_proxywidget.ui" line="58"/>
+        <source>HTTP</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sbi_proxywidget.ui" line="63"/>
+        <source>SOCKS5</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sbi_proxywidget.ui" line="74"/>
+        <location filename="../sbi_proxywidget.ui" line="220"/>
+        <source>Port:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sbi_proxywidget.ui" line="111"/>
+        <location filename="../sbi_proxywidget.ui" line="243"/>
+        <source>Username:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sbi_proxywidget.ui" line="121"/>
+        <location filename="../sbi_proxywidget.ui" line="253"/>
+        <source>Password:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sbi_proxywidget.ui" line="146"/>
+        <source>Use script for automatic configuration:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sbi_proxywidget.ui" line="153"/>
+        <source>System proxy configuration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sbi_proxywidget.ui" line="162"/>
+        <source>Proxy Auto-Config (.pac) file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sbi_proxywidget.ui" line="187"/>
+        <source>Manual configuration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sbi_proxywidget.ui" line="210"/>
+        <source>Server:</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
