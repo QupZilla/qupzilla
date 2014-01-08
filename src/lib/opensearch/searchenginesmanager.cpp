d61b72bd87d3db42b1e3a6708c097c31ff2ab4e6
nowrep <nowrep@gmail.com>
2014-01-08 10:39:41 +0100

    [SearchShortcuts] Fixed using search shortcuts longer than one character

--- src/lib/opensearch/searchenginesmanager.cpp
+++ src/lib/opensearch/searchenginesmanager.cpp
@@ -1,6 +1,6 @@
 /* ============================================================
 * QupZilla - WebKit based browser
-* Copyright (C) 2010-2013  David Rosca <nowrep@gmail.com>
+* Copyright (C) 2010-2014  David Rosca <nowrep@gmail.com>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -97,6 +97,7 @@ void SearchEnginesManager::loadSettings()
 SearchEngine SearchEnginesManager::engineForShortcut(const QString &shortcut)
 {
     Engine returnEngine;
+
     if (shortcut.isEmpty()) {
         return returnEngine;
     }
