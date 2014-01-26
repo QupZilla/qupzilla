7f6e5e40fe80173faa64eb081c7099dba1cfff2d
nowrep <nowrep@gmail.com>
2014-01-26 19:29:54 +0100

    Version 1.6.1

--- src/plugins/KWalletPasswords/kwalletplugin.cpp
+++ src/plugins/KWalletPasswords/kwalletplugin.cpp
@@ -59,7 +59,7 @@ void KWalletPlugin::unload()
 
 bool KWalletPlugin::testPlugin()
 {
-    return (QupZilla::VERSION == QLatin1String("1.6.0"));
+    return (QupZilla::VERSION == QLatin1String("1.6.1"));
 }
 
 QTranslator* KWalletPlugin::getTranslator(const QString &locale)
--- src/plugins/MouseGestures/mousegesturesplugin.cpp
+++ src/plugins/MouseGestures/mousegesturesplugin.cpp
@@ -65,7 +65,7 @@ bool MouseGesturesPlugin::testPlugin()
 {
     // Let's be sure, require latest version of QupZilla
 
-    return (QupZilla::VERSION == QLatin1String("1.6.0"));
+    return (QupZilla::VERSION == QLatin1String("1.6.1"));
 }
 
 QTranslator* MouseGesturesPlugin::getTranslator(const QString &locale)
