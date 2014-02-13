d005505dbaeaa2c641c50f7e5cfb74c1e59d4918
nowrep <nowrep@gmail.com>
2014-02-13 20:16:20 +0100

    FreeBSD build patches
    Imported from http://svnweb.freebsd.org/ports/head/www/qupzilla/files/
    See #1206

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -21,7 +21,7 @@
 #include <QMessageBox> // For QT_REQUIRE_VERSION
 #include <iostream>
 
-#if defined(Q_OS_LINUX) || defined(__GLIBC__)
+#if defined(Q_OS_LINUX) || defined(__GLIBC__) || defined(__FreeBSD__)
 #include <signal.h>
 #include <execinfo.h>
 
@@ -111,7 +111,7 @@ void qupzilla_signal_handler(int s)
         break;
     }
 }
-#endif // defined(Q_OS_LINUX) || defined(__GLIBC__)
+#endif // defined(Q_OS_LINUX) || defined(__GLIBC__) || defined(__FreeBSD__)
 
 #ifndef Q_OS_WIN
 #if (QT_VERSION < 0x050000)
@@ -175,7 +175,7 @@ int main(int argc, char* argv[])
     QApplication::setGraphicsSystem("raster"); // Better overall performance on X11
 #endif
 
-#if defined(Q_OS_LINUX) || defined(__GLIBC__)
+#if defined(Q_OS_LINUX) || defined(__GLIBC__) || defined(__FreeBSD__)
     signal(SIGSEGV, qupzilla_signal_handler);
     signal(SIGPIPE, qupzilla_signal_handler);
 #endif
