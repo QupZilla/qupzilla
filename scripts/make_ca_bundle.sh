ea5e59f18f26241e1a20a028fd4ea4a1bebf8d12
nowrep <nowrep@gmail.com>
2014-03-14 12:27:10 +0100

    [CaBundle] Update bundle + fix make_ca_bundle.sh script
    make_ca_bundle.sh was actually appending all certs to the file.
    As a result, there were duplicated certificates after multiple runs
    of the script.

--- scripts/make_ca_bundle.sh
+++ scripts/make_ca_bundle.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # It will probably work only for Debian based distros
 
-cat /usr/share/ca-certificates/*/*.crt > ../src/lib/data/data/ca-bundle.crt
-cat /etc/ssl/certs/*.pem >> ../src/lib/data/data/ca-bundle.crt
+cat /etc/ssl/certs/*.pem > ../src/lib/data/data/ca-bundle.crt
 
 exit 0
