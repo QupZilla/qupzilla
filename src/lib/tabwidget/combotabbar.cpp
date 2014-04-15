97d32a11915d36762143ea33147194adac83b127
David Rosca <nowrep@gmail.com>
2014-04-15 09:58:12 +0200

    [Mac] Drawing QTabBar without actual object crashes on Mac
    [ci skip]

--- src/lib/tabwidget/combotabbar.cpp
+++ src/lib/tabwidget/combotabbar.cpp
@@ -636,6 +636,7 @@ void ComboTabBar::paintEvent(QPaintEvent* ev)
 {
     QWidget::paintEvent(ev);
 
+#ifndef Q_OS_MAC
     // Draw tabbar base even on parts of ComboTabBar that are not directly QTabBar
     QPainter p(this);
     QStyleOptionTabBarBaseV2 opt;
@@ -664,6 +665,7 @@ void ComboTabBar::paintEvent(QPaintEvent* ev)
         opt.rect.setWidth(scrollButtonWidth);
         style()->drawPrimitive(QStyle::PE_FrameTabBarBase, &opt, &p);
     }
+#endif
 }
 
 int ComboTabBar::comboTabBarPixelMetric(ComboTabBar::SizeType sizeType) const
