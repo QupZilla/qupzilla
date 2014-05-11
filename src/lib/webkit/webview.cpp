86b689c7748b4d17f5e13350bfb50e8c6449b2a5
AlexTalker <alextalker@openmailbox.org>
2014-05-11 22:29:22 +0400

    Added yandex translater in menu by right mouse key.Already can't work.

--- src/lib/webkit/webview.cpp
+++ src/lib/webkit/webview.cpp
@@ -1056,6 +1056,9 @@ void WebView::createPageContextMenu(QMenu* menu, const QPoint &pos)
             QByteArray langCode = mApp->currentLanguage().left(2).toUtf8();
             const QUrl gturl = QUrl::fromEncoded("http://translate.google.com/translate?sl=auto&tl=" + langCode + "&u=" + QUrl::toPercentEncoding(url().toEncoded()));
             menu->addAction(QIcon(":icons/sites/translate.png"), tr("Translate page"), this, SLOT(openUrlInSelectedTab()))->setData(gturl);
+
+            const QUrl yturl = QUrl::fromEncoded("https://translate.yandex.ru/translate?url=" + QUrl::toPercentEncoding(url().toEncoded()) + "&lang=" + langCode);
+            menu->addAction(QIcon(":icons/sites/translate.png"), tr("Translate page"), this, SLOT(openUrlInSelectedTab()))->setData(yturl);
         }
 
         menu->addSeparator();
@@ -1143,6 +1146,13 @@ void WebView::createSelectedTextContextMenu(QMenu* menu, const QWebHitTestResult
     connect(gtwact, SIGNAL(triggered()), this, SLOT(openUrlInSelectedTab()));
     connect(gtwact, SIGNAL(ctrlTriggered()), this, SLOT(openUrlInBackgroundTab()));
     menu->addAction(gtwact);
+
+    QUrl yandexTranslateUrl = QUrl(QString("http://translate.yandex.ru/?lang=%1&text=%2").arg(langCode, selectedText));
+    Action* ytwact = new Action(QIcon(":icons/sites/translate.png"), tr("Google Translate"));
+    ytwact->setData(yandexTranslateUrl);
+    connect(ytwact, SIGNAL(triggered()), this, SLOT(openUrlInSelectedTab()));
+    connect(ytwact, SIGNAL(ctrlTriggered()), this, SLOT(openUrlInBackgroundTab()));
+    menu->addAction(ytwact);
 
     Action* dictact = new Action(QIcon::fromTheme("accessories-dictionary"), tr("Dictionary"));
     dictact->setData(QUrl("http://" + (!langCode.isEmpty() ? langCode + "." : langCode) + "wiktionary.org/wiki/Special:Search?search=" + selectedText));
