72438755a2b05412b12ff8560add6a412e2b35fa
David Rosca <nowrep@gmail.com>
2015-02-08 09:55:11 +0100

    AboutDialog: Show QtWebEngine version
    For now, it will only show version of Qt

--- src/lib/other/aboutdialog.cpp
+++ src/lib/other/aboutdialog.cpp
@@ -75,9 +75,7 @@ void AboutDialog::showAbout()
                            Qz::VERSION
 #endif
                        );
-#if QTWEBENGINE_DISABLED
-        m_aboutHtml += tr("<b>WebKit version %1</b></p>").arg(qWebKitVersion());
-#endif
+        m_aboutHtml += tr("<b>QtWebEngine version %1</b></p>").arg(QT_VERSION_STR);
         m_aboutHtml += QString("<p>&copy; %1 %2<br/>").arg(Qz::COPYRIGHT, Qz::AUTHOR);
         m_aboutHtml += tr("<small>Build time: %1 </small></p>").arg(Qz::BUILDTIME);
         m_aboutHtml += QString("<p><a href=%1>%1</a></p>").arg(Qz::WWWADDRESS);
