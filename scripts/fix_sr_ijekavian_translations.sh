b8b2510faeadb0ce6efb46e67258a42ee6c75835
David Rosca <nowrep@gmail.com>
2015-11-02 19:19:27 +0100

    Add fix_sr_ijekavian_translations.sh script

---
+++ scripts/fix_sr_ijekavian_translations.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd ..
+
+mv src/plugins/AccessKeysNavigation/translations/sr@{I,i}jekavian.ts
+mv src/plugins/AutoScroll/translations/sr@{I,i}jekavian.ts
+mv src/plugins/FlashCookieManager/translations/sr@{I,i}jekavian.ts
+mv src/plugins/GnomeKeyringPasswords/translations/sr@{I,i}jekavian.ts
+mv src/plugins/GreaseMonkey/translations/sr@{I,i}jekavian.ts
+mv src/plugins/KWalletPasswords/translations/sr@{I,i}jekavian.ts
+mv src/plugins/MouseGestures/translations/sr@{I,i}jekavian.ts
+mv src/plugins/PIM/translations/sr@{I,i}jekavian.ts
+mv src/plugins/StatusBarIcons/translations/sr@{I,i}jekavian.ts
+mv src/plugins/TabManager/translations/sr@{I,i}jekavian.ts
+mv translations/sr@{I,i}jekavian.ts
+
+echo "Done"
