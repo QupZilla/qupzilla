73150201a1eecdc881b1778f48c7eb69a5785e83
David Rosca <nowrep@gmail.com>
2015-11-24 18:23:14 +0100

    UserAgentManager: Update list of known user agents
    Closes #1779

--- src/lib/other/useragentmanager.cpp
+++ src/lib/other/useragentmanager.cpp
@@ -26,7 +26,7 @@ UserAgentManager::UserAgentManager(QObject* parent)
     : QObject(parent)
     , m_usePerDomainUserAgent(false)
 {
-    m_fakeUserAgent = QString("Mozilla/5.0 (%1) AppleWebKit/%2 (KHTML, like Gecko) Chrome/10.0 Safari/%2").arg(QzTools::operatingSystem(), BrowserWindow::WEBKITVERSION);
+    m_fakeUserAgent = QString("Mozilla/5.0 (%1) AppleWebKit/%2 (KHTML, like Gecko) Chrome/30.0 Safari/%2").arg(QzTools::operatingSystem(), BrowserWindow::WEBKITVERSION);
 }
 
 void UserAgentManager::loadSettings()
