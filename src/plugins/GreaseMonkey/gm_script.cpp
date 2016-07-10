78834bd9fa178a1a850dbf13f7c6344cae930bf8
Martii <thalamew@q.com>
2016-07-10 16:38:22 -0600

    WIN: Add CR to detection
    Saving in Notepad++ with Windows line ending prevents script injection
    
    Post fix for #1964 and some vague references at https://github.com/QupZilla/qupzilla/commit/ce67c7a455a58b3cf64491054688e4c9e9593c3a

--- src/plugins/GreaseMonkey/gm_script.cpp
+++ src/plugins/GreaseMonkey/gm_script.cpp
@@ -238,7 +238,7 @@ void GM_Script::parseScript()
 
     const QString fileData = QString::fromUtf8(file.readAll());
 
-    QzRegExp rx(QSL("(?:^|\\n)// ==UserScript==(.*)\\n// ==/UserScript==(?:\\n|$)"));
+    QzRegExp rx(QSL("(?:^|[\\r\\n])// ==UserScript==(.*)[\\r\\n]// ==/UserScript==(?:[\\r\\n]|$)"));
     rx.indexIn(fileData);
     QString metadataBlock = rx.cap(1).trimmed();
 
