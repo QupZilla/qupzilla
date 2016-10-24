5d60037e71062a23cf5982f24b24c7d9de0bd3f8
David Rosca <nowrep@gmail.com>
2016-10-24 17:25:44 +0200

    Update translations

--- src/plugins/AccessKeysNavigation/translations/zh_HK.ts
+++ src/plugins/AccessKeysNavigation/translations/zh_HK.ts
@@ -4,12 +4,12 @@
     <message>
         <location filename="../akn_settings.ui" line="14"/>
         <source>Access Keys Navigation</source>
-        <translation>訪問密鑰導覽</translation>
+        <translation>鍵盤網頁導覽</translation>
     </message>
     <message>
         <location filename="../akn_settings.ui" line="20"/>
         <source>&lt;h1&gt;Access Keys Navigation&lt;/h1&gt;</source>
-        <translation>&lt;h1&gt;訪問密鑰導覽&lt;/h1&gt;</translation>
+        <translation>&lt;h1&gt;鍵盤網頁導覽&lt;/h1&gt;</translation>
     </message>
     <message>
         <location filename="../akn_settings.ui" line="33"/>
@@ -29,12 +29,12 @@
     <message>
         <location filename="../akn_settings.ui" line="51"/>
         <source>Double press</source>
-        <translation>雙按</translation>
+        <translation>按兩下</translation>
     </message>
     <message>
         <location filename="../akn_settings.ui" line="58"/>
         <source>Key for showing access keys:</source>
-        <translation type="unfinished"/>
+        <translation>鍵盤網頁導覽啟用鍵：</translation>
     </message>
     <message>
         <location filename="../akn_settings.ui" line="103"/>
