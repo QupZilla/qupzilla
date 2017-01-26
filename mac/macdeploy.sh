d2ac65cd66c050c77d94b914bb0aa56a18967e1b
Martii <thalamew@q.com>
2017-01-26 15:34:29 -0700

    Prompt for known, missing, Qt plugins libraries
    * Make an environment variable for the plugins target folder
    * y or n to inject Qt dependencies
    
    OS X 10.12.3 (Sierra)
    
    Applies to #2180

--- mac/macdeploy.sh
+++ mac/macdeploy.sh
@@ -7,6 +7,7 @@
 
 MACDEPLOYQT="macdeployqt"
 LIBRARY_NAME="libQupZilla.2.dylib"
+PLUGINS="QupZilla.app/Contents/Resources/plugins"
 
 if [ -n "$1" ]; then
  MACDEPLOYQT=$1
@@ -20,20 +21,33 @@ cd bin
 cp $LIBRARY_NAME QupZilla.app/Contents/MacOS/
 
 # copy all QupZilla plugins into bundle
-test -d QupZilla.app/Contents/Resources/plugins || mkdir QupZilla.app/Contents/Resources/plugins
-cp plugins/*.dylib QupZilla.app/Contents/Resources/plugins/
+test -d $PLUGINS || mkdir $PLUGINS
+cp plugins/*.dylib $PLUGINS/
 
 # fix libQupZilla
 install_name_tool -change $LIBRARY_NAME @executable_path/$LIBRARY_NAME QupZilla.app/Contents/MacOS/QupZilla
 
 # fix plugins
-for plugin in QupZilla.app/Contents/Resources/plugins/*.dylib
+for plugin in $PLUGINS/*.dylib
 do
  install_name_tool -change $LIBRARY_NAME @executable_path/$LIBRARY_NAME $plugin
 done
 
-# copy additional Qt native plugin(s) into bundle
-cp $QTDIR/plugins/iconengines/libqsvgicon.dylib QupZilla.app/Contents/Resources/plugins/
+# prompt and optionally copy additional Qt native plugin(s) into bundle
+echo -n "Do you wish to redistribute known, missing, Qt plugins (y/n)? "
+old_stty_cfg=$(stty -g)
+stty raw -echo
+answer=$( while ! head -c 1 | grep -i '[yn]'; do true; done )
+stty $old_stty_cfg
+if echo "$answer" | grep -iq "^y"; then
+  printf '\nCopying known, missing, Qt native plugins to target...\n'
+
+  cp $QTDIR/plugins/iconengines/libqsvgicon.dylib $PLUGINS
+else
+  printf '\nChecking for prior deploy image libraries at target...\n'
+
+  rm $PLUGINS/libqsvgicon.dylib
+fi
 
 # run macdeployqt
 $MACDEPLOYQT QupZilla.app
@@ -41,4 +55,3 @@ $MACDEPLOYQT QupZilla.app
 # create final dmg image
 cd ../mac
 ./create_dmg.sh
-
