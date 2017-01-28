ea7f4dd58ff58cc48f65a8697051a85d5045070e
David Rosca <nowrep@gmail.com>
2017-01-28 14:31:47 +0100

    HistoryManager: Pressing Del key now removes selected entries

--- src/lib/history/historymanager.cpp
+++ src/lib/history/historymanager.cpp
@@ -1,6 +1,6 @@
 /* ============================================================
-* QupZilla - WebKit based browser
-* Copyright (C) 2010-2016  David Rosca <nowrep@gmail.com>
+* QupZilla - Qt web browser
+* Copyright (C) 2010-2017 David Rosca <nowrep@gmail.com>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -81,6 +81,17 @@ void HistoryManager::clearHistory()
     }
 
     mApp->history()->clearHistory();
+}
+
+void HistoryManager::keyPressEvent(QKeyEvent *event)
+{
+    switch (event->key()) {
+    case Qt::Key_Delete:
+        ui->historyTree->removeSelectedItems();
+        break;
+    }
+
+    QWidget::keyPressEvent(event);
 }
 
 void HistoryManager::search(const QString &searchText)
--- src/lib/history/historymanager.h
+++ src/lib/history/historymanager.h
@@ -1,6 +1,6 @@
 /* ============================================================
-* QupZilla - WebKit based browser
-* Copyright (C) 2010-2014  David Rosca <nowrep@gmail.com>
+* QupZilla - Qt web browser
+* Copyright (C) 2010-2017 David Rosca <nowrep@gmail.com>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -66,6 +66,8 @@ private slots:
     void clearHistory();
 
 private:
+    void keyPressEvent(QKeyEvent *event) override;
+
     BrowserWindow* getWindow();
 
     Ui::HistoryManager* ui;
