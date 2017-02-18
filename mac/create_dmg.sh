79bf6dcc495159b629a753a02897a868fea632a3
Martii <thalamew@q.com>
2017-02-18 12:37:15 -0700

    Refactor OS X dmg creation
    * Make the template dynamic instead of static hosted in project and modified
    * Add some quotes for possible future spaced naming error protection
    * Use AppleScript to script window style of dmg
    * Add bin/QupZilla.dmg (builds) to .gitignore so it doesn't get pushed accidentally

--- mac/create_dmg.sh
+++ mac/create_dmg.sh
@@ -1,26 +1,61 @@
 #!/bin/bash
 
+TMP=/tmp
 BUNDLE_PATH=bin
 test -d bin || BUNDLE_PATH=../bin
 
-echo "We just want to make sure it's not in use"
-hdiutil detach /tmp/tmp-release-qupzilla
+WORK_TEMPLATE=tmp-QupZilla-release.dmg.sparseimage
 
-echo "Creating writable DMG from template"
-hdiutil convert QupZilla-template.dmg -format UDRW -o /tmp/tmp-qupzilla.dmg
+# NOTE: Value must currently match -volname exactly or an error happens in AppleScript with Finder
+VOLUME_TEMPLATE=QupZilla
 
-echo "Attach DMG for write"
-hdiutil attach /tmp/tmp-qupzilla.dmg -mountpoint /tmp/tmp-qupzilla-release
+echo "Ensuring working disk image template is not in use…"
+hdiutil detach $TMP/$WORK_TEMPLATE
 
-echo "Copying…"
-cp -fpR $BUNDLE_PATH/QupZilla.app/Contents /tmp/tmp-qupzilla-release/QupZilla.app
+echo "Creating writable working disk image template…"
+hdiutil create -size 200m "$TMP/$WORK_TEMPLATE" -type SPARSE -fs HFS+ -volname "QupZilla"
 
-hdiutil detach /tmp/tmp-qupzilla-release
+  echo "Attaching working disk image template for modification…"
+  hdiutil attach "$TMP/$WORK_TEMPLATE" -mountpoint "$TMP/$VOLUME_TEMPLATE"
 
-echo "Creating final compressed(bz2) DMG…"
-rm $BUNDLE_PATH/QupZilla.dmg
-hdiutil convert /tmp/tmp-qupzilla.dmg -format UDBZ -o $BUNDLE_PATH/QupZilla.dmg
+    echo "Creating background image folder…"
+    mkdir "$TMP/$VOLUME_TEMPLATE/.background"
 
-echo "Removing temp files…"
-rm /tmp/tmp-qupzilla.dmg
-exit
+    echo "Copying background image…"
+    cp images/qupzilla-dmg-background.png "$TMP/$VOLUME_TEMPLATE/.background/"
+
+    echo "Creating volume icon set…"
+    iconutil -c icns images/qupzilla-dmg-icon.iconset -o "$TMP/$VOLUME_TEMPLATE/.VolumeIcon.icns"
+
+    echo "Creating application reference folder…"
+    mkdir "$TMP/$VOLUME_TEMPLATE/QupZilla.app"
+
+    echo "Creating symbolic link to global Applications folder…"
+    ln -s /Applications "$TMP/$VOLUME_TEMPLATE/Applications"
+
+    echo "Setting some proprietary window modifications here with AppleScript…"
+    osascript create_dmg.scpt
+
+    sleep 5
+
+    echo "Registering that a custom icon has been set…"
+    SetFile -a C "$TMP/$VOLUME_TEMPLATE"
+
+    echo "Copying application bundle contents…"
+    cp -fpR "$BUNDLE_PATH/QupZilla.app/Contents" "$TMP/$VOLUME_TEMPLATE/QupZilla.app"
+
+    echo "Blessing folder to automatically open on mount…"
+    bless -folder "$TMP/$VOLUME_TEMPLATE" --openfolder "$TMP/$VOLUME_TEMPLATE"
+
+  echo "Detaching working disk image template from write…"
+  hdiutil detach "$TMP/$VOLUME_TEMPLATE"
+
+echo "Compacting working disk image…"
+hdiutil compact "$TMP/$WORK_TEMPLATE"
+
+echo "Converting working disk image to read only…"
+rm "$BUNDLE_PATH/QupZilla.dmg"
+hdiutil convert "$TMP/$WORK_TEMPLATE" -format UDBZ -o "$BUNDLE_PATH/QupZilla.dmg"
+
+echo  "Cleaning up"
+rm "$TMP/$WORK_TEMPLATE"
