QupZilla/qupzilla

commit 92b49be0c2e2e4195e92fc180650056e4e3bff74

Don't leak web page
Signed-off-by: Anthony Fieroni <bvbfan@abv.bg>

src/lib/webengine/webview.cpp
@@ -146,6 +146,10 @@ void WebView::setPage(WebPage *page)
         return;
     }
 
+    if (m_page) {
+        m_page->deleteLater(); // we are the parent, see below
+    }
+
     m_page = page;
     m_page->setParent(this);
     QWebEngineView::setPage(m_page);

commit bd9a33d6f90a3c6229a56a0cee0816b92e66a937

Fix crash when opening target __blank links
Regression from 92b49be0c2e2e4195e92fc180650056e4e3bff74

Closes #2335

src/lib/webengine/webview.cpp
@@ -147,7 +147,8 @@ void WebView::setPage(WebPage *page)
     }
 
     if (m_page) {
-        m_page->deleteLater(); // we are the parent, see below
+        m_page->setView(nullptr);
+        m_page->deleteLater();
     }
 
     m_page = page;