ddf7f6f83633c511cdfbba146def8b57c112276d
Razi Alavizadeh <s.r.alavizadeh@gmail.com>
2017-09-28 17:08:14 +0330

    TabManager: Bump version to 0.8

--- src/plugins/TabManager/tabmanagerplugin.cpp
+++ src/plugins/TabManager/tabmanagerplugin.cpp
@@ -51,7 +51,7 @@ PluginSpec TabManagerPlugin::pluginSpec()
     spec.name = "Tab Manager";
     spec.info = "Simple yet powerful tab manager for QupZilla";
     spec.description = "Adds ability to managing tabs and windows";
-    spec.version = "0.7.0";
+    spec.version = "0.8.0";
     spec.author = "Razi Alavizadeh <s.r.alavizadeh@gmail.com>";
     spec.icon = QPixmap(":tabmanager/data/tabmanager.png");
     spec.hasSettings = true;
