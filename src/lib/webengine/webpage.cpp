2efe797a4d1676d498c42492ad1bb2394f414b05
David Rosca <nowrep@gmail.com>
2018-01-14 09:16:30 +0100

    WebPage: Remove workaround for crash in setHtml

--- src/lib/webengine/webpage.cpp
+++ src/lib/webengine/webpage.cpp
@@ -365,7 +365,6 @@ void WebPage::renderProcessTerminated(QWebEnginePage::RenderProcessTerminationSt
         page.replace(QL1S("%LI-2%"), tr("Try reloading the page or closing some tabs to make more memory available."));
         page.replace(QL1S("%RELOAD-PAGE%"), tr("Reload page"));
         page = QzTools::applyDirectionToPage(page);
-        load(url()); // Workaround for QtWebEngine crash
         setHtml(page.toUtf8(), url());
     });
 }
