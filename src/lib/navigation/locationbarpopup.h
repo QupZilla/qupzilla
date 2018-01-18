97a2a5826c340a27d9f798536639bb589b673195
Konstantin Savun <diablopc@linuxforum.tech>
2018-01-18 19:58:22 +0200

    All copyrights at once =)

--- src/lib/navigation/locationbarpopup.h
+++ src/lib/navigation/locationbarpopup.h
@@ -1,6 +1,6 @@
 /* ============================================================
 * QupZilla - WebKit based browser
-* Copyright (C) 2010-2014  Franz Fellner <alpine.art.de@googlemail.com>
+* Copyright (C) 2010-2018  Franz Fellner <alpine.art.de@googlemail.com>
 *                          David Rosca <nowrep@gmail.com>
 *
 * This program is free software: you can redistribute it and/or modify
--- src/lib/tools/qztools.h
+++ src/lib/tools/qztools.h
@@ -1,6 +1,6 @@
 /* ============================================================
 * QupZilla - WebKit based browser
-* Copyright (C) 2010-2016 David Rosca <nowrep@gmail.com>
+* Copyright (C) 2010-2018 David Rosca <nowrep@gmail.com>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
